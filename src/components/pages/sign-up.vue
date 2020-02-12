d3e3f09684f3134acf8c6fa367ed87f5502c249c
Drego31 <dominik.betka@gmail.com>
2020-02-12 10:24:44 +0100

    Add example pages #13

---
+++ src/components/pages/sign-up.vue
@@ -0,0 +1,17 @@
+<template>
+  <t-page>
+    sign up
+    <router-link to="/">Back</router-link>
+  </t-page>
+</template>
+
+<script>
+import TPage from 'components/templates/page'
+
+export default {
+  name: 'p-sign-up',
+  components: {
+    TPage,
+  },
+}
+</script>
