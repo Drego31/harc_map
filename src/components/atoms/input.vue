e5bfef4339d53babe137fb68325b1513644172bb
Drego31 <dominik.betka@gmail.com>
2020-02-12 14:22:17 +0100

    Add mixin with vModel and use it in a-input #28

---
+++ src/components/atoms/input.vue
@@ -0,0 +1,12 @@
+<template>
+  <input class="a-input" v-model="vModel"/>
+</template>
+
+<script>
+import { mixins } from 'components/mixins/base'
+
+export default {
+  name: 'a-input',
+  mixins: [mixins.vModel],
+}
+</script>
