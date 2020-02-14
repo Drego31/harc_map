a02a75998011fb63b2a2ec406f4882959f49ae4e
Drego31 <dominik.betka@gmail.com>
2020-02-14 11:46:29 +0100

    Add all neccessary field to sign-in #14

--- src/components/pages/sign-in.vue
+++ src/components/pages/sign-in.vue
@@ -1,15 +1,26 @@
 <template>
   <t-page>
     <div class="a-title">Logowanie</div>
+    <div class="f-pb-1">
+      <a-input placeholder="e-mail" />
+      <a-input placeholder="hasło" />
+      <a-button>Dalej</a-button>
+    </div>
+    <a-button>Przypomnij hasło</a-button>
+    <a-button>Zarejestruj</a-button>
   </t-page>
 </template>
 
 <script>
-import TPage from 'components/templates/page'
+import TPage from 'templates/page'
+import AInput from 'atoms/input'
+import AButton from 'atoms/button'
 
 export default {
   name: 'p-sign-in',
   components: {
+    AButton,
+    AInput,
     TPage,
   },
 }
