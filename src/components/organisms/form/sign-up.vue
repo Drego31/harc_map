cef668ada34a97d0d4ae0901c46d027153e8dfc9
Pawel <bedpaw97@gmail.com>
2020-02-15 13:31:08 +0100

    Create template to  O-sign-up and add all required field to sign-up page

---
+++ src/components/organisms/form/sign-up.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="f-pb-1">
+    <a-input
+      v-model="values.email"
+      placeholder="e-mail"
+      type="email"
+      :disabled="blockForm"
+    />
+    <a-input
+      v-model="values.password"
+      placeholder="hasło"
+      type="password"
+      :disabled="blockForm"
+    />
+    <a-button
+      :disabled="blockForm"
+    >
+      {{ isSending ? 'Sprawdzamy...' : (message ? message : 'Dalej') }}
+    </a-button>
+  </div>
+</template>
+
+<script>
+import AInput from 'atoms/input'
+import AButton from 'atoms/button'
+import { api } from 'api/index'
+export default {
+  name: 'o-form-sign-in',
+  components: {
+    AInput,
+    AButton,
+  },
+  data: () => ({
+    values: {
+      email: '',
+      password: '',
+    },
+    blockForm: false,
+    isSending: false,
+    message: '',
+  }),
+}
+</script>
--- src/components/pages/sign-up.vue
+++ src/components/pages/sign-up.vue
@@ -1,64 +1,22 @@
 <template>
   <t-page>
-    <div>
-      {{ this.user }}
-      <h1>Sign Up:</h1>
-      <p>Patrol name: {{ user.patrolName }}</p>
-      <a-input
-        v-model="user.patrolName"
-        placeholder="Type patrol name"
-      />
-      <p>Email: {{ user.email }}</p>
-      <a-input
-        v-model="user.email"
-        placeholder="Type your email"
-      />
-      <p>Password: {{ user.password }}</p>
-      <a-input
-        v-model="user.password"
-        placeholder="Type your password"
-      />
-      <p>Repeat password: {{ passwordRepeat }}</p>
-      <a-input
-        v-model="passwordRepeat"
-        placeholder="Repeat your password"
-      />
-      <a-button @click="addNewUser(this.user)" class="f-40">
-        Create account
-      </a-button>
-      <router-link to="/">
-        <a-button class="f-40">
-          Back to log in
-        </a-button>
-      </router-link>
-    </div>
+    <div class="a-title">Zarejestruj się</div>
+    <o-form-sign-up/>
+    <a-button @click="$router.push('/')">Wróć do logowania</a-button>
   </t-page>
 </template>
 
 <script>
 import TPage from 'templates/page'
-import AInput from 'atoms/input'
+import OFormSignUp from 'organisms/form/sign-up'
 import AButton from 'atoms/button'
 
 export default {
   name: 'p-sign-up',
   components: {
     AButton,
-    AInput,
+    OFormSignUp,
     TPage,
-  },
-  data: () => ({
-    user: {
-      email: '',
-      patrolName: '',
-      password: '',
-    },
-    passwordRepeat: '',
-  }),
-  methods: {
-    addNewUser (user) {
-      console.log(user)
-    },
   },
 }
 </script>
