32f0cce47a1cde0b439509b05e326b725981c4c1
Pawel <bedpaw97@gmail.com>
2020-02-15 17:08:17 +0100

    Add all required fields and func in sign-up view and refactor mockApi #15

--- src/components/organisms/form/sign-up.vue
+++ src/components/organisms/form/sign-up.vue
@@ -12,7 +12,26 @@
       type="password"
       :disabled="blockForm"
     />
+    <a-input
+      v-model="repeatedPassword"
+      placeholder="powtórz hasło"
+      type="password"
+      :disabled="blockForm"
+    />
+    <a-input
+      v-model="values.patrolName"
+      placeholder="nazwa patrolu"
+      type="email"
+      :disabled="blockForm"
+    />
+    <a-input
+      v-model="values.eventCode"
+      placeholder="kod wydarzenia"
+      type="email"
+      :disabled="blockForm"
+    />
     <a-button
+      @click="signUp()"
       :disabled="blockForm"
     >
       {{ isSending ? 'Sprawdzamy...' : (message ? message : 'Dalej') }}
@@ -24,6 +43,7 @@
 import AInput from 'atoms/input'
 import AButton from 'atoms/button'
 import { api } from 'api/index'
+
 export default {
   name: 'o-form-sign-in',
   components: {
@@ -34,10 +54,59 @@ export default {
     values: {
       email: '',
       password: '',
+      patrolName: '',
+      eventCode: '',
     },
+    repeatedPassword: '',
     blockForm: false,
     isSending: false,
     message: '',
   }),
+  methods: {
+    checkValues () {
+      const { email, password, patrolName, eventCode } = this.values
+      const validEmail = email.length >= 5
+      const validPassword = password.length >= 5 && password === this.repeatedPassword
+      const validPatrolName = patrolName.length >= 3
+      const validEventCode = eventCode === '111'
+      return validEmail && validPassword && validPatrolName && validEventCode
+    },
+    setMessage (text) {
+      return new Promise(resolve => {
+        this.message = text
+        setTimeout(() => {
+          this.message = ''
+          this.blockForm = false
+          resolve()
+        }, 2000)
+      })
+    },
+    onSignUp () {
+      this.setMessage('Konto utoworzono pomyślnie - kliknij w link aktywacyjny na swojej skrzynce mailowej')
+        .then(() => {
+          this.$router.push('/')
+        })
+      this.isSending = false
+    },
+    onError () {
+      this.setMessage('Spróbuj ponownie...')
+      this.isSending = false
+    },
+    onInvalidValues () {
+      this.setMessage('Błędne dane')
+      this.isSending = false
+    },
+    signUp () {
+      this.isSending = true
+      this.blockForm = true
+      if (this.checkValues()) {
+        api.signUp(this.values)
+          .then(this.onSignUp)
+          .catch(this.onError)
+      } else {
+        this.onInvalidValues()
+      }
+    },
+  },
 }
 </script>
