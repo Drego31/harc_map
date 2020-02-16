bb25d5a0106da14747dac1d5cf995ae5dd9a6be2
Pawel <bedpaw97@gmail.com>
2020-02-16 09:08:27 +0100

    Correct mixin-form #15

--- src/components/mixins/form.js
+++ src/components/mixins/form.js
@@ -1,20 +1,22 @@
 export const formMixin = {
-  setMessage (text) {
-    return new Promise(resolve => {
-      this.message = text
-      setTimeout(() => {
-        this.message = ''
-        this.blockForm = false
-        resolve()
-      }, 2000)
-    })
-  },
-  onError () {
-    this.setMessage('Spróbuj ponownie...')
-    this.isSending = false
-  },
-  onInvalidValues () {
-    this.setMessage('Błędne dane')
-    this.isSending = false
+  methods: {
+    setMessage (text) {
+      return new Promise(resolve => {
+        this.message = text
+        setTimeout(() => {
+          this.message = ''
+          this.blockForm = false
+          resolve()
+        }, 2000)
+      })
+    },
+    onError () {
+      this.setMessage('Spróbuj ponownie...')
+      this.isSending = false
+    },
+    onInvalidValues () {
+      this.setMessage('Błędne dane')
+      this.isSending = false
+    },
   },
 }
