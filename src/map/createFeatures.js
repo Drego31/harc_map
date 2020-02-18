c99c1d8a7174cb252d36f7ed136255f3ae4e0882
Adam Dominik <adam.dominik89@gmail.com>
2020-02-18 11:34:13 +0100

    points on the map are created with different colors

--- src/map/createFeatures.js
+++ src/map/createFeatures.js
@@ -5,6 +5,17 @@ import Point from 'ol/geom/Point'
 import * as Projection from 'ol/proj'
 import { map } from 'src/map'
 import { Fill, RegularShape, Stroke, Style } from 'ol/style'
+import { MAP_POINTS } from '../utils/macros/mapPointTypes'
+
+const getStroke = (type, width = 2) => {
+  return new Stroke({
+    color: MAP_POINTS[type.toString()].strokeColor,
+    width,
+  })
+}
+const getFill = (type) => {
+  return new Fill({ color: MAP_POINTS[type.toString()].fillColor })
+}
 
 export function createFeatures ({ list }) {
   for (const point of list) {
@@ -12,17 +23,15 @@ export function createFeatures ({ list }) {
     const lon = point.position.longitude
     const type = point.type
 
-    const stroke = new Stroke({
-      color: 'black',
-      width: 2,
-    })
-    const fill = new Fill({ color: 'red' })
+    const stroke = getStroke(type)
+    const fill = getFill(type)
+
     const position = Projection.fromLonLat([lon, lat])
 
     const feature = new Feature({
       geometry: new Point(position),
     })
-    console.log({ type })
+    // console.log({ type })
     feature.setStyle(new Style({
       image: new RegularShape({
         fill: fill,
---
+++ src/utils/macros/mapPointTypes.js
@@ -0,0 +1,18 @@
+export const MAP_POINTS = {
+  '1': {
+    strokeColor: 'black',
+    fillColor: 'blue',
+  },
+  '2': {
+    strokeColor: 'black',
+    fillColor: 'yellow',
+  },
+  '3': {
+    strokeColor: 'black',
+    fillColor: 'red',
+  },
+  '4': {
+    strokeColor: 'blue',
+    fillColor: 'red',
+  },
+}
