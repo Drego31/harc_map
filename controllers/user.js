5fd86b9c1daffb150566665388d138224006315f
Paweł Jurkiewicz <pawel.j@henouser.pl>
2020-02-23 17:59:23 +0100

    Creating database module #47

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const database = require('../lib/mongodb');
 
-router.get('/',(req, res) => {
+router.get('/', (req, res) => {
 	database.read("users").then(result => {
 		res.send(result);
 	}).catch(error => {
@@ -10,22 +10,22 @@ router.get('/',(req, res) => {
 	})
 });
 
-router.get('/add',(req, res) => {
-	database.creat("users", [{
+router.get('/add', (req, res) => {
+	database.creat("users", [ {
 		email: 'another@example.example',
 		password: 'example2',
 		eventId: 1234567890
-	}]).then(result => {
+	} ]).then(result => {
 		res.send(result);
 	}).catch(error => {
 		res.status(404).send(error);
 	})
 });
 
-router.get('/remove',(req, res) => {
-	database.remove("users", [{
-		email: 'another@example.example',
-	}]).then(result => {
+router.get('/remove', (req, res) => {
+	database.remove("users", {
+		email: { $eq: 'another@example.example' }
+	}).then(result => {
 		res.send(result);
 	}).catch(error => {
 		res.status(404).send(error);
