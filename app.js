ddbf447d1843ac7cf66b9310a036324a129db8bb
Paweł Jurkiewicz <pawel.j@henouser.pl>
2020-02-23 17:05:40 +0100

    Creating database module #47

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
+/**
+ * HarcMap
+ * HarcMap Server
+ */
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-
 const config = require('./config/system.config');
 const userController = require('./controllers/user');
 
 // create express app instance
 const app = express();
-// server port
-const port = config.app.port;
-const key = config.app.key;
-const cert = config.app.cert;
 
 /**
- * @description Routing
+ * Routing
  */
 // static files
 app.use(express.static('public'));
 // user controller
 app.use('/user', userController);
 
-// https
+/**
+ * Https server startup
+ */
 https.createServer({
-	key: fs.readFileSync(key),
-	cert: fs.readFileSync(cert),
-}, app).listen(port, () => {
-	console.log(`Start HTTPS server. Listening on port ${port}`);
+	key: fs.readFileSync(config.key),
+	cert: fs.readFileSync(config.cert),
+}, app).listen(config.port, () => {
+	console.log(`\x1b[32m\nHTTPS server \x1b[1mstarted\x1b[0m\x1b[32m [${config.port}]\n\x1b[0m`);
 });
