60e4add4763e8880f97b1db9cc6cc815779a3ff2
Pawel <bedpaw97@gmail.com>
2020-02-27 17:42:45 +0100

    Create inputs and methods for o-change-password #79

--- src/components/organisms/form/change-password.vue
+++ src/components/organisms/form/change-password.vue
@@ -1,9 +1,72 @@
 <template>
-  <div> CHANGE PASSWORD</div>
+  <div class="f-pb-1">
+    <div v-if="!formSend">
+      <a-input
+        :disabled="blockForm"
+        placeholder="Type your new password"
+        v-model="password"
+        type="password"
+      />
+      <a-input
+        :disabled="blockForm"
+        placeholder="Repeat your new password"
+        v-model="repeatedPassword"
+        type="password"
+      />
+      <a-button-submit
+        :disabled="blockForm"
+        :is-sending="isSending"
+        :message="message"
+        @click="changePassword()"
+      />
+    </div>
+    <p v-else>Twoje hasło zostało pomyślnie zmienione</p>
+  </div>
 </template>
 
 <script>
+import AButtonSubmit from 'atoms/button/submit';
+import AInput from 'atoms/input';
+import { api } from 'api/index';
+import { mixins } from 'mixins/base';
+
 export default {
   name: 'change-password.vue',
+  mixins: [mixins.form],
+  components: {
+    AButtonSubmit,
+    AInput,
+  },
+  data: () => ({
+    password: '',
+    repeatedPassword: '',
+    blockForm: false,
+    isSending: false,
+    message: '',
+    formSend: false,
+  }),
+  methods: {
+    checkValues () {
+      return this.password.length >= 5 && this.password === this.repeatedPassword;
+    },
+    changePassword () {
+      this.isSending = true;
+      this.blockForm = true;
+      if (this.checkValues()) {
+        api.changePassword(this.password)
+          .then(this.onChangePassword)
+          .catch(this.onError);
+      } else {
+        this.onInvalidValues();
+      }
+    },
+    onChangePassword () {
+      this.setMessage('Zakończono pomyślnie!')
+        .then(() => {
+          this.formSend = true;
+        });
+      this.isSending = false;
+    },
+  },
 };
 </script>
