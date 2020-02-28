96d8111d07ac2139803aab16ba7c202808a4a82b
Paweł Jurkiewicz <pawel.j@henouser.pl>
2020-02-28 12:43:16 +0100

    Zmiany w konfiguracji aplikacji. Dodanie logow. #80

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
 /**
  * HarcMap
- * HarcMap Server
  */
+const logs = require('./lib/logs');
+// Environment
+const ENV_DEVELOPMENT = process.env.NODE_ENV === 'development';
+logs.init(ENV_DEVELOPMENT);
+
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-// Configs
-const config = require('./config/system.config');
+const utils = require('./lib/utils');
+const appConfig = utils.getSystemConfig().app;
+
+// Controllers
 const userController = require('./controllers/user');
 
 // Create express app instance
 const app = express();
-const ENV_DEVELOPMENT = process.env.NODE_ENV === 'development';
-// Log environment
-console.log(`\u{1F680} App run in \x1b[1m${ENV_DEVELOPMENT ? 'DEVELOPMENT' : 'PRODUCTION'}\x1b[0m mode`);
 
-// Configuration
+/**
+ * Configuration
+  */
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 // Session
-const sessionConfig = config.session;
+const sessionConfig = appConfig.session;
 if (ENV_DEVELOPMENT) {
+  // Session cookie for development
   Object.assign(sessionConfig.cookie, {
     secure: false,
     httpOnly: false,
     sameSite: false,
   });
-}
-app.use(expressSession(sessionConfig));
 
-// Access Control Allow Origin for development purpose
-if (ENV_DEVELOPMENT) {
+  // Access Control Allow Origin for development purpose
   app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Headers', '*');
     next();
   });
 }
+app.use(expressSession(sessionConfig));
 
 /**
  * Routing
@@ -47,7 +52,7 @@ if (ENV_DEVELOPMENT) {
 // static files
 // maxAge - Cache-Control header in milliseconds
 app.use(express.static('public', {
-  maxAge: ENV_DEVELOPMENT ? 0 : config.cacheValue,
+  maxAge: ENV_DEVELOPMENT ? 0 : appConfig.cacheValue,
 }));
 // user controller
 app.use('/user', userController);
@@ -60,8 +65,8 @@ app.get('*', (req, res) => {
  * Https server startup
  */
 https.createServer({
-  key: fs.readFileSync(config.key),
-  cert: fs.readFileSync(config.cert),
-}, app).listen(config.port, () => {
-  console.log(`   \x1b[32mHTTPS server \x1b[1mstarted\x1b[0m\x1b[32m [${config.port}]\n\x1b[0m`);
+  key: fs.readFileSync(appConfig.key),
+  cert: fs.readFileSync(appConfig.cert),
+}, app).listen(appConfig.port, () => {
+  console.log(`   \x1b[32mHTTPS server \x1b[1mstarted\x1b[0m\x1b[32m [${appConfig.port}]\n\x1b[0m`);
 });
