bf2a353ddfe7f40d3d7c21e8544e29d9109f7259
Drego31 <dominik.betka@gmail.com>
2020-03-02 16:00:11 +0100

    First version of bottom menu (footer) #91

---
+++ src/components/organisms/footer.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="o-footer"
+  > <!--    v-if="isLogin"-->
+    <a-button-icon
+      class="f-footer"
+    >
+      <span>
+        <icon-home :size="32"/>
+      </span>
+    </a-button-icon>
+    <a-button-icon
+      class="f-footer"
+    >
+      <icon-clock :size="32"/>
+    </a-button-icon>
+    <a-button-icon
+      class="f-footer"
+    >
+      <icon-star :size="32"/>
+    </a-button-icon>
+    <a-button-icon
+      class="f-footer"
+    >
+      <icon-map :size="32"/>
+    </a-button-icon>
+    <a-button-icon
+      class="f-footer"
+      @click="toggle()"
+    >
+      <icon-menu :size="32"/>
+    </a-button-icon>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+import IconMap from 'icons/Map.vue';
+import IconMenu from 'icons/Menu.vue';
+import IconStar from 'icons/Star.vue';
+import IconClock from 'icons/Clock.vue';
+import IconHome from 'icons/Home.vue';
+import AButtonIcon from 'atoms/button/icon';
+
+export default {
+  name: 'o-footer',
+  components: {
+    AButtonIcon,
+    IconMap,
+    IconMenu,
+    IconStar,
+    IconClock,
+    IconHome,
+  },
+  computed: {
+    ...mapGetters('user', [
+      'isLogin',
+    ]),
+  },
+  methods: {
+    ...mapMutations('menu', [
+      'toggle',
+    ]),
+  },
+};
+</script>
