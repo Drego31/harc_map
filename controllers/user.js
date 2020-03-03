01e7187301c872569927d67fec20b0973b91d8be
Filip Betka <filbetka@gmail.com>
2020-03-03 21:59:28 +0100

    Created mock API for `/user` address.

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const database = require('../lib/mongodb');
+const validator = require('./validator');
+// const database = require('../lib/mongodb');
 
+function getJsonRequest (request) {
+  let body = '';
+  request.on('data', chunk => body += chunk);
+  request.on('end', () => {
+    try {
+      return JSON.parse(body);
+    } catch (error) {
+      throw validator.ValidateCodes.IS_NOT_JSON;
+    }
+  });
+}
+
+router.post('/', (request, response) => {
+  response.send({
+    user: 'example@example.pl',
+    error: null,
+  });
+});
+
+router.post('/remind/', (request, response) => {
+  response.send({
+    user: 'example@example.pl',
+    error: null,
+  });
+});
+
+router.put('/remind/', (request, response) => {
+  response.send({
+    user: 'example@example.pl',
+    error: null,
+  });
+});
+
+router.post('/login/', (request, response) => {
+  response.send({
+    user: 'example@example.pl',
+    teamName: 'example team name',
+    collectedPointsIds: [1, 2, 3],
+    eventId: '1234',
+    error: null,
+  });
+});
+
+router.delete('/login/', (request, response) => {
+  response.send({
+    user: 'example@example.pl',
+    error: null,
+  });
+});
+
+/*
 router.get('/', (req, res) => {
   database.read('users').then(result => {
     res.send(result);
@@ -31,5 +83,6 @@ router.get('/remove', (req, res) => {
     res.status(404).send(error);
   });
 });
+*/
 
 module.exports = router;
