ed49cfc117e1405472020c8d1dd2d8185b87acef
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-03-05 16:28:01 +0100

    Create basic functions and elements for home view #97

--- src/components/organisms/home.vue
+++ src/components/organisms/home.vue
@@ -1,20 +1,42 @@
 <template>
-  <div>
-    HOME
+  <div class="f-flex f-flex-col f-flex-al-center">
+    <div>
+      HARC RUN CZY JAKOŚ TAK
+    </div>
+    <div>
+      <component
+        :is="iconStar"
+        :size="40"
+      />
+    </div>
+    <div>
+      {{collectedPointsIds.length}}
+    </div>
+    <div>
+      {{teamName}}
+    </div>
   </div>
 </template>
 
 <script>
 
+import { mapGetters } from 'vuex';
+import IconStar from 'icons/Star.vue';
+
 export default {
   name: 'o-home',
   components: {
+    IconStar,
   },
   data: () => ({
+    iconStar: IconStar,
   }),
   computed: {
+    ...mapGetters('user', [
+      'teamName', 'collectedPointsIds',
+    ]),
   },
-  methods: {
-  },
+  methods: {},
 };
+
 </script>
