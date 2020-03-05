0b0a7bd2b90edf5f383d3234d7167a73770aaa4e
Drego31 <dominik.betka@gmail.com>
2020-03-05 21:27:38 +0100

    Add icon for password input #90

--- src/components/molecules/input.vue
+++ src/components/molecules/input.vue
@@ -4,7 +4,7 @@
       :id="id"
       class="a-input"
       :class="additionalClasses"
-      :type="type"
+      :type="getType"
       v-model="vModel"
     />
     <label
@@ -14,6 +14,18 @@
     >
       {{ placeholder }}
     </label>
+    <icon-eye
+      v-if="isPassword && showPassword === false"
+      class="a-icon f-input"
+      :size="26"
+      @click="showPassword = true"
+    />
+    <icon-eye-off
+      v-if="isPassword && showPassword"
+      class="a-icon f-input"
+      :size="26"
+      @click="showPassword = false"
+    />
     <div
       class="a-assist"
       :class="{ 'f-error': error }"
@@ -25,10 +37,16 @@
 
 <script>
 import { mixins } from 'mixins/base';
+import IconEye from 'icons/Eye';
+import IconEyeOff from 'icons/EyeOff';
 
 export default {
   name: 'm-input',
   mixins: [mixins.vModel],
+  components: {
+    IconEye,
+    IconEyeOff,
+  },
   props: {
     placeholder: {
       type: String,
@@ -49,17 +67,28 @@ export default {
   },
   data: () => ({
     id: '',
+    showPassword: false,
   }),
   mounted () {
     const randomNumber = Math.floor(Math.random() * 10000);
     this.id = 'id-input-' + randomNumber;
   },
   computed: {
+    isPassword () {
+      return this.type === 'password';
+    },
     additionalClasses () {
       return {
         'f-filled': this.vModel !== '',
         'f-error': this.error,
       };
+    },
+    getType () {
+      if (this.isPassword) {
+        return this.showPassword ? '' : this.type;
+      } else {
+        return this.type;
+      }
     },
   },
 };
