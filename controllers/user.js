1088f1fb2abdcb3c64f445f6c94e533f998b802d
Paweł Jurkiewicz <pawel.j@henouser.pl>
2020-03-16 18:05:28 +0100

    Development "/user" endpoint #102

--- controllers/user.js
+++ controllers/user.js
@@ -50,6 +50,8 @@ router.post('/', (req, res) => {
                 accountType: 'common',
                 accountIsActive: false,
                 activationKey: utils.getRandomString(),
+                forgotKey: null,
+                forgotTimestamp: null,
                 accountCreated: Date.now(),
                 collectedPointsIds: [],
               };
@@ -159,11 +161,11 @@ router.get('/activation/:key', (req, res) => {
   database.read('users', { activationKey: key })
     .then(result => {
       if (result && !result.accountIsActive) {
-        console.log(result);
-        database.update('users', { _id: database.ObjectId(result._id) }, {
-            accountIsActive: true,
-            activationKey: null,
-          })
+        const activationUpdateData = {
+          accountIsActive: true,
+          activationKey: null,
+        };
+        database.update('users', { _id: database.ObjectId(result._id) }, activationUpdateData)
           .then(result => {
             // updated
             if (result) {
@@ -200,21 +202,22 @@ router.get('/activation/:key', (req, res) => {
     });
 });
 
+/**
+ * Remind password
+ * Send email with remind link
+ */
+router.post('/remind', (req, res) => {
+  const { user } = req.body;
+});
+
 // TODO remind
 
-// router.post('/', (request, response) => {
+// router.post('/remind/', (request, response) => {
 //   response.send({
 //     user: 'example@example.pl',
 //     error: null,
 //   });
 // });
-
-router.post('/remind/', (request, response) => {
-  response.send({
-    user: 'example@example.pl',
-    error: null,
-  });
-});
 
 router.put('/remind/', (request, response) => {
   response.send({
@@ -222,80 +225,5 @@ router.put('/remind/', (request, response) => {
     error: null,
   });
 });
-
-// router.post('/login/', (request, response) => {
-//   response.send({
-//     user: 'example@example.pl',
-//     teamName: 'example team name',
-//     collectedPointsIds: [1, 2, 3],
-//     eventId: '1234',
-//     error: null,
-//   });
-// });
-
-// router.delete('/login/', (request, response) => {
-//   response.send({
-//     user: 'example@example.pl',
-//     error: null,
-//   });
-// });
-
-/*
-router.get('/', (req, res) => {
-  database.read('users').then(result => {
-    res.json({isAuthenticated: req.isAuthenticated(), user: req.user});
-  }).catch(error => {
-    res.status(404).send(error);
-  });
-});
-
-router.post('/', (req, res, next) => {
-  passport.authenticate('local', (error, user) => {
-    if (error || !user) {
-      // failed login
-      res.status(401).json({
-        email: null,
-        error,
-      });
-    } else {
-      req.login(user, error => {
-        // error with setting session
-        if (error) {
-          res.status(500).json({
-            email: null,
-            error: 'unhandled session error',
-          });
-        } else {
-          const dataSendToUser = Object.assign({}, user);
-          delete dataSendToUser.id;
-          res.json(dataSendToUser);
-        }
-      });
-    }
-  })(req, res, next);
-});
-
-router.get('/add', (req, res) => {
-  database.creat('users', [{
-    email: 'another@example.example',
-    password: 'example2',
-    eventId: 1234567890,
-  }]).then(result => {
-    res.send(result);
-  }).catch(error => {
-    res.status(404).send(error);
-  });
-});
-
-router.get('/remove', (req, res) => {
-  database.remove('users', {
-    email: { $eq: 'another@example.example' },
-  }).then(result => {
-    res.send(result);
-  }).catch(error => {
-    res.status(404).send(error);
-  });
-});
-*/
 
 module.exports = router;
