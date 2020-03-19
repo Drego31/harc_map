514d9b25fd15e3b8a281dad53495ce2f4e3e871d
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-03-19 21:47:03 +0100

    Create basic functions in collectedPoints view #120

--- src/components/organisms/collected-points.vue
+++ src/components/organisms/collected-points.vue
@@ -1,20 +1,52 @@
 <template>
   <div>
-    Collected points
+    <div class="a-title">
+      Zebrane punkty:
+    </div>
+    <div v-for="point in pointCategories"
+         :key=point.pointValue class="a-label f-point-container.sass">
+      <icon-star :size="24"/>
+      {{point.difficultName}}: {{getCollectedPointsLengthByPointValue(point.pointValue)}}
+    </div>
   </div>
 </template>
 
 <script>
+import { pointsForDatabase } from '../../../points';
+import { mapGetters } from 'vuex';
+import IconStar from 'icons/Star.vue';
 
 export default {
   name: 'o-collected-points',
   components: {
+    IconStar,
   },
   data: () => ({
+    pointCategories: [
+      {
+        difficultName: 'Łatwe',
+        pointValue: 1,
+      },
+      {
+        difficultName: 'Średnie',
+        pointValue: 2,
+      },
+      {
+        difficultName: 'Trudne',
+        pointValue: 3,
+      },
+    ],
   }),
   computed: {
+    // ...mapGetters('user', [
+    //   'collectedPointsIds',
+    // ]),
   },
   methods: {
+    getCollectedPointsLengthByPointValue (value) {
+      // return this.collectedPointsIds.filter(point => point.categoryId === value).length;
+      return this.pointsForDatabase().filter(point => point.categoryId === value).length;
+    },
   },
 };
 </script>
