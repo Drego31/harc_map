4a5c2d4e491b6ac7f9ec2f89b14b89b351fd1ca6
Paweł Jurkiewicz <pawel.j@henouser.pl>
2020-03-27 12:20:10 +0100

    Cleaning "/user" endpoint. Errors codes #102

--- controllers/user.js
+++ controllers/user.js
@@ -6,323 +6,280 @@ const utils = require('../lib/utils');
 const passport = require('passport');
 const mail = require('../lib/mail');
 const systemConfig = require('../config/system.config');
+const path = require('path');
+
+// Codes for errorsCodes
+const errorsCodes = validator.ValidateCodes;
 
 // TODO Validation
-// TODO error codes
-// TODO functions for same responses
 
 /**
- * User login
+ * @description Function sending error response and log them
+ * @param response {object} - "http" server response object
+ * @param httpStatus {number} - HTTP State Code to send
+ * @param errorCode {number} - error number
+ * @param [errorMsg] {string} - error message
  */
-router.post('/login', (req, res, next) => {
-  console.log(req.isAuthenticated());
-  if (req.isAuthenticated()) {
-    const { _id } = req.user;
-    database.read('users', { _id: database.ObjectId(_id) })
-      .then((userData) => {
-        const { user, userEvents, teamName, collectedPointsIds } = userData;
-        res.send({
-          user,
-          teamName,
-          collectedPointsIds,
-          eventId: userEvents[0],
-          error: null,
-        });
-      })
-      .catch(error => {
-        res.status(500).send({
-          user: null,
-          error,
-        });
-      });
-  } else {
-    passport.authenticate('local', (error, userData) => {
-      if (error || !userData) {
-        // failed login
-        // TODO error name
-        res.status(401).send({
-          user: null,
-          error,
-        });
-      } else {
-        req.login(userData, error => {
-          // error with setting session
-          if (error) {
-            // TODO error name
-            res.status(500).send({
-              user: null,
-              error: 'unhandled session error',
-            });
-          } else {
-            const { user, teamName, collectedPointsIds, userEvents } = userData;
-            res.send({
-              user,
-              teamName,
-              collectedPointsIds,
-              eventId: userEvents[0],
-            });
-          }
-        });
-      }
-    })(req, res, next);
-  }
-});
-
-/**
- * User logout
- */
-router.delete('/login', (req, res) => {
-  const { user } = req.user;
-  req.logout();
-  res.send({
-    user: user,
-    error: null,
+function __sendError (response, httpStatus, errorCode, errorMsg) {
+  response.status(httpStatus).send({
+    user: null,
+    error: errorCode,
   });
-});
+  if (errorMsg) console.trace(errorMsg);
+}
 
 /**
- * User Registration
+ * @description User login, logout
  */
-router.post('/registration', (req, res) => {
-  const { user, password, userTeam, eventId } = req.body;
-  // check if eventId is correct(exist)
-  database.read('events', { eventId })
-    .then(result => {
-      // event exist - user can be creat
-      if (result) {
-        // check if user and teamName doesn't already exist
-        database.read('users', { $or: [{ user }, { userTeam }] })
-          .then(result => {
-            // exist
-            if (result) {
-              // TODO error name
-              res.send({
-                user,
-                error: 'user exist',
-              });
+router.route('/login')
+  /**
+   * @description Login
+   */
+  .post((req, res, next) => {
+    // Checking if user is already logged
+    if (req.isAuthenticated()) {
+      // Data from session
+      const { user, userTeam, collectedPointsIds, userEvents } = req.user;
+      res.send({
+        user,
+        userTeam,
+        collectedPointsIds,
+        eventId: userEvents[0],
+        error: null,
+      });
+    } else {
+      passport.authenticate('local', (error, userData) => {
+        if (error || !userData) {
+          // failed login
+          __sendError(res, 401, errorsCodes.UNAUTHORIZED_ACCESS, error);
+        } else {
+          req.login(userData, error => {
+            // error with setting session
+            if (error) {
+              __sendError(res, 500, errorsCodes.SESSION_ERROR, error);
             } else {
-              // user doesn't exist, we can create new
-              const newUserData = {
+              const { user, userTeam, collectedPointsIds, userEvents } = userData;
+              res.send({
                 user,
                 userTeam,
-                password: utils.getSHA(password),
-                userEvents: [eventId],
-                accountType: 'common',
-                accountIsActive: false,
-                activationKey: utils.getRandomString(),
-                forgotKey: null,
-                forgotTimestamp: null,
-                accountCreated: Date.now(),
-                collectedPointsIds: [],
-              };
-              database.create('users', [newUserData])
-                .then(() => {
-                  mail.accountActivation(user, newUserData.activationKey)
-                    .then(() => {
-                      // TODO error name
-                      // added
-                      res.send({
-                        user,
-                        error: null,
-                      });
-                    })
-                    .catch(error => {
-                      res.send({
-                        user,
-                        error,
-                      });
-                    });
-                })
-                .catch(error => {
-                  // TODO error name
-                  res.status(500).send({
-                    user,
-                    error: error,
-                  });
-                });
-            }
-          })
-          .catch(error => {
-            // TODO error name
-            res.status(500).send({
-              user,
-              error: error,
-            });
-          });
-      } else {
-        // TODO error name
-        res.status(401).send({
-          user,
-          error: 'eventId not exist',
-        });
-      }
-    })
-    .catch(error => {
-      // TODO error name
-      res.status(500).send({
-        user,
-        error,
-      });
-    });
-});
-
-/**
- * User activation account
- */
-router.get('/activation/:key', (req, res) => {
-  const { key } = req.params;
-  database.read('users', { activationKey: key })
-    .then(result => {
-      if (result && !result.accountIsActive) {
-        const activationUpdateData = {
-          accountIsActive: true,
-          activationKey: null,
-        };
-        database.update('users', { _id: database.ObjectId(result._id) }, activationUpdateData)
-          .then(result => {
-            // updated
-            if (result) {
-              res.redirect(302, '/');
-            } else {
-              // TODO error code
-              res.status(500).send({
-                user: null,
-                error: 'undefined server error',
+                collectedPointsIds,
+                eventId: userEvents[0],
+                error: null,
               });
             }
-          })
-          .catch(error => {
-            // TODO error code
-            res.status(500).send({
-              user: null,
-              error: error,
-            });
           });
-      } else {
-        // TODO error code
-        res.status(500).send({
-          user: null,
-          error: 'undefined server error',
-        });
-      }
-    })
-    .catch(error => {
-      // TODO error code
-      res.status(500).send({
-        user: null,
-        error,
-      });
+        }
+      })(req, res, next);
+    }
+  })
+  /**
+   * @description Logout
+   */
+  .delete((req, res) => {
+    // Data from client
+    const { user } = req.user;
+    // Logout from session
+    req.logout();
+    res.send({
+      user: user,
+      error: null,
     });
-});
+  });
 
 /**
- * Forgot password
- * Send email with remind link
+ * @description Registration
  */
-router.post('/remind', (req, res) => {
-  const { email } = req.body;
-  database.read('users', { user: email })
-    .then(() => {
-      const forgotKey = utils.getRandomString();
-      // Update user data with forgot key and forgot timestamp
-      database.update('users', { user: email }, { forgotKey: forgotKey, forgotTimestamp: Date.now() })
-        .then(() => {
-          // send mail with forgot key
-          mail.resetPassword(email, forgotKey)
+router.route('/registration')
+  .post((req, res) => {
+    // Data from client
+    const { user, password, userTeam, eventId } = req.body;
+    // check if eventId is correct(exist)
+    database.read('events', { eventId })
+      .then(result => {
+        // event exist - user can be creat
+        if (result) {
+          // check if user and userTeam doesn't already exist
+          database.read('users', { $or: [{ user }, { userTeam }] })
+            .then(result => {
+              // exist
+              if (result) {
+                __sendError(res, 400, errorsCodes.USER_EXIST);
+              } else {
+                // user doesn't exist, we can create new
+                const newUserData = {
+                  user,
+                  userTeam,
+                  password: utils.getSHA(password),
+                  userEvents: [eventId],
+                  accountType: 'common',
+                  accountIsActive: false,
+                  activationKey: utils.getRandomString(),
+                  forgotKey: null,
+                  forgotTimestamp: null,
+                  accountCreated: Date.now(),
+                  collectedPointsIds: [],
+                };
+                database.create('users', [newUserData])
+                  .then(() => {
+                    mail.accountActivation(user, newUserData.activationKey)
+                      .then(() => {
+                        // User created
+                        res.send({
+                          user,
+                          error: null,
+                        });
+                      })
+                      .catch(error => {
+                        __sendError(res, 500, errorsCodes.MAIL_UNKNOWN_ERROR, error);
+                      });
+                  })
+                  .catch(error => {
+                    __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+                  });
+              }
+            })
+            .catch(error => {
+              __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+            });
+        } else {
+          __sendError(res, 400, errorsCodes.EVENT_ID_NOT_EXIST, error);
+        }
+      })
+      .catch(error => {
+        __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+      });
+  });
+
+/**
+ * @description User activation account
+ */
+router.route('/activation/:key')
+  .get((req, res) => {
+    // Data from client
+    const { key } = req.params;
+    database.read('users', { activationKey: key })
+      .then(result => {
+        if (result && !result.accountIsActive) {
+          const activationUpdateData = {
+            accountIsActive: true,
+            activationKey: null,
+          };
+          database.update('users', { _id: database.ObjectId(result._id) }, activationUpdateData)
+            .then(result => {
+              // Successfully updated
+              if (result) {
+                res.redirect(302, '/');
+              } else {
+                __sendError(res, 500, errorsCodes.DATABASE_NO_RESULT_ERROR);
+              }
+            })
+            .catch(error => {
+              __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+            });
+        } else {
+          __sendError(res, 401, errorsCodes.ACCOUNT_IS_INACTIVE);
+        }
+      })
+      .catch(error => {
+        __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+      });
+  });
+
+/**
+ * @description Forgot password
+ */
+router.route('/remind')
+  /**
+   * @description Send email with remind link
+   */
+  .post((req, res) => {
+    // Data from client
+    const { email } = req.body;
+    database.read('users', { user: email })
+      .then(() => {
+        const forgotData = {
+          forgotKey: utils.getRandomString(),
+          forgotTimestamp: Date.now(),
+        };
+        // Update user data with forgot key and forgot timestamp
+        database.update('users', { user: email }, forgotData)
+          .then(() => {
+            // Sending mail
+            mail.resetPassword(email, forgotData.forgotKey)
+              .then(() => {
+                // Email successfully sent
+                res.send({
+                  user: email,
+                  error: null,
+                });
+              })
+              .catch(error => {
+                __sendError(res, 500, errorsCodes.MAIL_UNKNOWN_ERROR, error);
+              });
+          })
+          .catch(error => {
+            __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+          });
+      })
+      .catch(error => {
+        __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+      });
+  });
+
+/**
+ * @description Forgot password
+ */
+router.route('/remind/:key')
+  /**
+   * @description TODO Return HTML if user was ask for change password (not longer then 24h - systemConfig.app.passwordForgotTimeoutInMinutes)
+   */
+  .get((req, res) => {
+    // Data from client
+    const { key } = req.params;
+    database.read('users', { forgotKey: key })
+      .then((result) => {
+        if (result.forgotTimestamp >= Date.now() - (systemConfig.app.passwordForgotTimeoutInMinutes * 60 * 1000)) {
+          res.sendFile(path.resolve(__dirname, '../public/index.html'));
+        } else {
+          res.status(404).send();
+        }
+      })
+      .catch(() => {
+        res.status(404).send();
+      });
+  })
+  /**
+   * @description Change password in Database
+   */
+  .put((req, res) => {
+    // Data from client
+    const { password } = req.body;
+    const { key } = req.params;
+    database.read('users', { forgotKey: key })
+      .then((userData) => {
+        if (userData.forgotTimestamp >= Date.now() - (systemConfig.app.passwordForgotTimeoutInMinutes * 60 * 1000)) {
+          const updateData = {
+            password: utils.getSHA(password),
+            forgotKey: null,
+            forgotTimestamp: null,
+          };
+          database.update('users', { _id: database.ObjectId(userData._id) }, updateData)
             .then(() => {
-              // TODO error name
-              // added
               res.send({
-                user: email,
+                user: null,
                 error: null,
               });
             })
-            .catch(() => {
-              res.send({
-                user: email,
-                error: null,
-              });
+            .catch(error => {
+              __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
             });
-        })
-        .catch(() => {
-          res.send({
-            user: email,
-            error: null,
-          });
-        });
-    })
-    .catch(() => {
-      res.send({
-        user: email,
-        error: null,
+        } else {
+          res.status(404).send();
+        }
+      })
+      .catch(error => {
+        __sendError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
       });
-    });
-});
-
-/**
- * Forgot password
- * TODO Return HTML if user was ask for change password (not longer then 24h - systemConfig.app.passwordForgotTimeoutInMinutes)
- */
-router.get('/remind/:key', (req, res) => {
-  const { key } = req.params;
-  database.read('users', { forgotKey: key })
-    .then((result) => {
-      if (result.forgotTimestamp >= Date.now() - (systemConfig.app.passwordForgotTimeoutInMinutes * 60 * 1000)) {
-        res.send({
-          user: null,
-          error: null,
-        });
-      } else {
-        res.status(404).send({
-          user: null,
-          error: null,
-        });
-      }
-    })
-    .catch(() => {
-      res.status(404).send({
-        user: null,
-        error: null,
-      });
-    });
-});
-
-/**
- * Forgot password
- * Change password in Database
- */
-router.put('/remind/:key', (req, res) => {
-  const { password } = req.body;
-  const { key } = req.params;
-  database.read('users', { forgotKey: key })
-    .then((userData) => {
-      if (userData.forgotTimestamp >= Date.now() - (systemConfig.app.passwordForgotTimeoutInMinutes * 60 * 1000)) {
-        database.update('users', { _id: database.ObjectId(userData._id) }, { password: utils.getSHA(password) })
-          .then(() => {
-            res.send({
-              user: null,
-              error: null,
-            });
-          })
-          .catch(() => {
-            res.status(500).send({
-              user: null,
-              error: null,
-            });
-          });
-      } else {
-        res.status(404).send({
-          user: null,
-          error: null,
-        });
-      }
-    })
-    .catch(() => {
-      res.status(404).send({
-        user: null,
-        error: null,
-      });
-    });
-});
+  });
 
 module.exports = router;
