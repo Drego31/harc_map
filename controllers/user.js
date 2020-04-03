e1549fbcdf765edd90e07626f1246bc01e90b486
Paweł Jurkiewicz <pawel.j@henouser.pl>
2020-04-03 11:57:40 +0200

    Add validation to user endpoint #133

--- controllers/user.js
+++ controllers/user.js
@@ -46,41 +46,55 @@ router.route('/login')
         error: null,
       });
     } else {
-      passport.authenticate('local', (error, userData) => {
-        if (error || !userData) {
-          // failed login
-          utils.responseError(res, 401, errorsCodes.UNAUTHORIZED_ACCESS, error);
-        } else {
-          req.login(userData, error => {
-            // error with setting session
-            if (error) {
-              utils.responseError(res, 500, errorsCodes.SESSION_ERROR, error);
-            } else {
-              const { user, userTeam, collectedPointsIds, userEvents } = userData;
-              res.send({
-                user,
-                userTeam,
-                collectedPointsIds,
-                eventId: userEvents[0],
-                error: null,
-              });
-            }
-          });
-        }
-      })(req, res, next);
+      // User data validation
+      const requestBodyValidationError = validator.validate(
+        validator.methods.validateUserLoginPostRequest, req.body,
+      );
+      if (!requestBodyValidationError) {
+        passport.authenticate('local', (error, userData) => {
+          if (error || !userData) {
+            // failed login
+            utils.responseError(res, 401, error);
+          } else {
+            req.login(userData, error => {
+              // error with setting session
+              if (error) {
+                utils.responseError(res, 500, errorsCodes.SESSION_ERROR, error);
+              } else {
+                const { user, userTeam, collectedPointsIds, userEvents } = userData;
+                res.send({
+                  user,
+                  userTeam,
+                  collectedPointsIds,
+                  eventId: userEvents[0],
+                  error: null,
+                });
+              }
+            });
+          }
+        })(req, res, next);
+      } else {
+        utils.responseError(res, 400, requestBodyValidationError);
+      }
     }
   })
   /**
    * @description Logout
    */
   .delete((req, res) => {
-    // Data from client
-    const { user } = req.user;
-    // Logout from session
-    req.logout();
+    let user = null;
+    let error = null;
+    if (req.isAuthenticated()) {
+      // Data from client
+      user = req.user.user;
+      // Logout from session
+      req.logout();
+    } else {
+      error = validator.ValidateCodes.UNAUTHORIZED_ACCESS;
+    }
     res.send({
-      user: user,
-      error: null,
+      user,
+      error,
     });
   });
 
@@ -89,63 +103,71 @@ router.route('/login')
  */
 router.route('/registration')
   .post((req, res) => {
-    // Data from client
-    const { user, password, userTeam, eventId } = req.body;
-    // check if eventId is correct(exist)
-    database.read('events', { eventId })
-      .then(result => {
-        // event exist - user can be creat
-        if (result) {
-          // check if user and userTeam doesn't already exist
-          database.read('users', { $or: [{ user }, { userTeam }] })
-            .then(result => {
-              // exist
-              if (result) {
-                utils.responseError(res, 400, errorsCodes.USER_EXIST);
-              } else {
-                // user doesn't exist, we can create new
-                const newUserData = {
-                  user,
-                  userTeam,
-                  password: utils.getSHA(password),
-                  userEvents: [eventId],
-                  accountType: 'common',
-                  accountIsActive: false,
-                  activationKey: utils.getRandomString(),
-                  forgotKey: null,
-                  forgotTimestamp: null,
-                  accountCreated: Date.now(),
-                  collectedPointsIds: [],
-                };
-                database.create('users', [newUserData])
-                  .then(() => {
-                    mail.accountActivation(user, newUserData.activationKey)
-                      .then(() => {
-                        // User created
-                        res.send({
-                          user,
-                          error: null,
+    // User data validation
+    const requestBodyValidationError = validator.validate(
+      validator.methods.validateUserRegistrationPostRequest, req.body,
+    );
+    if (!requestBodyValidationError) {
+      // Data from client
+      const { user, password, userTeam, eventId } = req.body;
+      // check if eventId is correct(exist)
+      database.read('events', { eventId })
+        .then(result => {
+          // event exist - user can be creat
+          if (result) {
+            // check if user and userTeam doesn't already exist
+            database.read('users', { $or: [{ user }, { userTeam }] })
+              .then(result => {
+                // exist
+                if (result) {
+                  utils.responseError(res, 400, errorsCodes.USER_EXIST);
+                } else {
+                  // user doesn't exist, we can create new
+                  const newUserData = {
+                    user,
+                    userTeam,
+                    password: utils.getSHA(password),
+                    userEvents: [eventId],
+                    accountType: 'common',
+                    accountIsActive: false,
+                    activationKey: utils.getRandomString(),
+                    forgotKey: null,
+                    forgotTimestamp: null,
+                    accountCreated: Date.now(),
+                    collectedPointsIds: [],
+                  };
+                  database.create('users', [newUserData])
+                    .then(() => {
+                      mail.accountActivation(user, newUserData.activationKey)
+                        .then(() => {
+                          // User created
+                          res.send({
+                            user,
+                            error: null,
+                          });
+                        })
+                        .catch(error => {
+                          utils.responseError(res, 500, errorsCodes.MAIL_UNKNOWN_ERROR, error);
                         });
-                      })
-                      .catch(error => {
-                        utils.responseError(res, 500, errorsCodes.MAIL_UNKNOWN_ERROR, error);
-                      });
-                  })
-                  .catch(error => {
-                    utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-                  });
-              }
-            })
-            .catch(error => {
-              utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-            });
-        } else {
-          utils.responseError(res, 400, errorsCodes.EVENT_ID_NOT_EXIST);
-        }
-      })
-      .catch(error => {
-        utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-      });
+                    })
+                    .catch(error => {
+                      utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+                    });
+                }
+              })
+              .catch(error => {
+                utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+              });
+          } else {
+            utils.responseError(res, 400, errorsCodes.EVENT_ID_NOT_EXIST);
+          }
+        })
+        .catch(error => {
+          utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+        });
+    } else {
+      utils.responseError(res, 400, requestBodyValidationError);
+    }
   });
 
 /**
@@ -191,37 +213,45 @@ router.route('/remind')
    * @description Send email with remind link
    */
   .post((req, res) => {
-    // Data from client
-    const { email } = req.body;
-    database.read('users', { user: email })
-      .then(() => {
-        const forgotData = {
-          forgotKey: utils.getRandomString(),
-          forgotTimestamp: Date.now(),
-        };
-        // Update user data with forgot key and forgot timestamp
-        database.update('users', { user: email }, forgotData)
-          .then(() => {
-            // Sending mail
-            mail.resetPassword(email, forgotData.forgotKey)
-              .then(() => {
-                // Email successfully sent
-                res.send({
-                  user: email,
-                  error: null,
+    // User data validation
+    const requestBodyValidationError = validator.validate(
+      validator.methods.validateUserRemindPostRequest, req.body,
+    );
+    if (!requestBodyValidationError) {
+      // Data from client
+      const { email } = req.body;
+      database.read('users', { user: email })
+        .then(() => {
+          const forgotData = {
+            forgotKey: utils.getRandomString(),
+            forgotTimestamp: Date.now(),
+          };
+          // Update user data with forgot key and forgot timestamp
+          database.update('users', { user: email }, forgotData)
+            .then(() => {
+              // Sending mail
+              mail.resetPassword(email, forgotData.forgotKey)
+                .then(() => {
+                  // Email successfully sent
+                  res.send({
+                    user: email,
+                    error: null,
+                  });
+                })
+                .catch(error => {
+                  utils.responseError(res, 500, errorsCodes.MAIL_UNKNOWN_ERROR, error);
                 });
-              })
-              .catch(error => {
-                utils.responseError(res, 500, errorsCodes.MAIL_UNKNOWN_ERROR, error);
-              });
-          })
-          .catch(error => {
-            utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-          });
-      })
-      .catch(error => {
-        utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-      });
+            })
+            .catch(error => {
+              utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+            });
+        })
+        .catch(error => {
+          utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+        });
+    } else {
+      utils.responseError(res, 400, requestBodyValidationError);
+    }
   });
 
 /**
@@ -229,7 +259,7 @@ router.route('/remind')
  */
 router.route('/remind/:key')
   /**
-   * @description TODO Return HTML if user was ask for change password (not longer then 24h - systemConfig.app.passwordForgotTimeoutInMinutes)
+   * @description TODO Return HTML if user was ask for change password (not longer then - systemConfig.app.passwordForgotTimeoutInMinutes)
    */
   .get((req, res) => {
     // Data from client
@@ -250,34 +280,42 @@ router.route('/remind/:key')
    * @description Change password in Database
    */
   .put((req, res) => {
-    // Data from client
-    const { password } = req.body;
-    const { key } = req.params;
-    database.read('users', { forgotKey: key })
-      .then((userData) => {
-        if (__checkForgotTimeout(userData.forgotTimestamp)) {
-          const updateData = {
-            password: utils.getSHA(password),
-            forgotKey: null,
-            forgotTimestamp: null,
-          };
-          database.update('users', { _id: database.ObjectId(userData._id) }, updateData)
-            .then(() => {
-              res.send({
-                user: null,
-                error: null,
+    // User data validation
+    const requestBodyValidationError = validator.validate(
+      validator.methods.validateUserRemindPutRequest, req.body,
+    );
+    if (!requestBodyValidationError) {
+      // Data from client
+      const { password } = req.body;
+      const { key } = req.params;
+      database.read('users', { forgotKey: key })
+        .then((userData) => {
+          if (__checkForgotTimeout(userData.forgotTimestamp)) {
+            const updateData = {
+              password: utils.getSHA(password),
+              forgotKey: null,
+              forgotTimestamp: null,
+            };
+            database.update('users', { _id: database.ObjectId(userData._id) }, updateData)
+              .then(() => {
+                res.send({
+                  user: null,
+                  error: null,
+                });
+              })
+              .catch(error => {
+                utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
               });
-            })
-            .catch(error => {
-              utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-            });
-        } else {
-          res.status(404).send();
-        }
-      })
-      .catch(error => {
-        utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
-      });
+          } else {
+            res.status(404).send();
+          }
+        })
+        .catch(error => {
+          utils.responseError(res, 500, errorsCodes.DATABASE_DATA_ERROR, error);
+        });
+    } else {
+      utils.responseError(res, 400, requestBodyValidationError);
+    }
   });
 
 module.exports = router;
