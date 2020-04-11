f49b2a1feb7b944a16e3efb2ca98b64fb5756856
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-04-11 13:36:57 +0200

    Create m-list-element #120

---
+++ src/components/molecules/list-element.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="f-flex f-flex-row f-mb-2 f-flex-1 ">
+    <div>
+      <icon-star :size="64" class="a-icon" :class="point.imageColor"/>
+    </div>
+    <div class="a-list-tile">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconStar from 'vue-material-design-icons/Star';
+export default {
+  components: {
+    IconStar,
+  },
+  name: 'm-list-element',
+  props: {
+    point: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+
+  },
+};
+</script>
