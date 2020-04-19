1ebc742838f298b941bb5e3046ce0d6d91544e51
Filip Betka <filbetka@gmail.com>
2020-04-19 13:14:01 +0200

    #144: Added categories to get point.

--- controllers/eventPoint.js
+++ controllers/eventPoint.js
@@ -23,19 +23,31 @@ router.get('/', (request, response) => {
     pointId: json.pointId,
   };
 
-  database.read('event_' + json.eventId, filters)
-    .then(result => {
-      if (result === null) {
-        utils.responseDatabaseNoData(response, responseObject);
-        return;
-      }
-      responseObject = Object.assign(responseObject, result);
-      delete responseObject._id;
-      response.send(responseObject);
-    })
-    .catch(error => {
-      utils.responseDatabaseError(response, responseObject, error);
-    });
+  const readPoint = database.read('event_' + json.eventId, filters);
+  const readCategories = database.readMany('point_categories', {});
+
+  const ready = Promise.all([readPoint, readCategories]);
+  ready.then(result => {
+    let point = result[0];
+    if (point === null) {
+      utils.responseDatabaseNoData(response, responseObject);
+      return;
+    }
+
+    const categoryIndex = point.pointCategory - 1;
+    const category = result[1][categoryIndex];
+    point = Object.assign(point, category);
+    delete point.pointCategory;
+    delete point.categoryId;
+    delete point._id;
+
+    responseObject = Object.assign(responseObject, point);
+    response.send(responseObject);
+  });
+
+  ready.catch(error => {
+    utils.responseDatabaseError(response, responseObject, error);
+  });
 });
 
 router.post('/', (request, response) => {
