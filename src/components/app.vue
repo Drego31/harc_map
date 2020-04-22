19642d86de6ae8668f077cd60f395b86dd73343b
Drego31 <dominik.betka@gmail.com>
2020-04-22 17:42:43 +0200

    #148 Add loader organism and use it while checking session

--- src/components/app.vue
+++ src/components/app.vue
@@ -6,6 +6,10 @@
     </div>
     <o-footer/>
     <o-menu/>
+    <!--    <o-loading :class="{'f-hide': !isLoading}"/>-->
+    <transition name="fade">
+      <o-loading v-show="isLoading"/>
+    </transition>
   </div>
 </template>
 
@@ -13,19 +17,28 @@
 import OHeader from 'organisms/header';
 import OMenu from 'organisms/menu';
 import OFooter from 'organisms/footer';
+import OLoading from 'organisms/loading';
 import { api } from 'api/index';
 import { ROUTES } from 'utils/macros/routes';
+import { mapGetters } from 'vuex';
+import { promise } from 'utils/promise';
 
 export default {
   components: {
-    OFooter,
-    OMenu,
     OHeader,
+    OMenu,
+    OFooter,
+    OLoading,
   },
   mounted () {
     api.checkYourLoginSession()
       .then(data => this.$store.dispatch('user/signIn', data))
-      .then(() => this.$router.push(ROUTES.start.path));
+      .then(() => this.$router.push(ROUTES.start.path))
+      .then(() => promise.timeout(1000))
+      .then(() => this.$store.commit('loader/setIsLoading', false));
+  },
+  computed: {
+    ...mapGetters('loader', ['isLoading']),
   },
 };
 </script>
---
+++ src/components/organisms/loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="o-loading">
+    <span class="a-loader">
+      <img class="a-img f-loader f-big" src="/img/zhp.png" alt="logo"/>
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'o-loading',
+};
+</script>
