60e97f671cd4ddc04f1ed431f2d35c5b80e4e499
Filip Betka <filbetka@gmail.com>
2020-05-01 16:07:12 +0200

    #144: Created put point controller with Endpoint class.

--- controllers/eventPoint.js
+++ controllers/eventPoint.js
@@ -75,56 +75,44 @@ class PostRequestService extends Endpoint {
 
 }
 
+class PutRequestService extends Endpoint {
 
+  databasePart () {
+    const json = this.getRequestJson();
+    const toUpdate = {
+      pointId: json.point.pointId,
+      pointType: json.point.pointType,
+      pointName: json.point.pointName,
+      pointExpirationTime: json.point.pointExpirationTime,
+      pointCollectionTime: json.point.pointCollectionTime,
+      pointLongitude: json.point.pointLongitude,
+      pointLatitude: json.point.pointLatitude,
+      pointCategory: json.point.pointCategory,
+    };
 
+    const pointsCollection = 'event_' + json.eventId;
+    const eventFilter = { eventId: json.eventId };
+    const pointFilter = { pointId: json.point.pointId };
 
-function PutRequestDatabasePart (request, response, responseObject) {
+    return database.read('events', eventFilter)
 
-  const json = request.body;
-  const filters = {
-    pointId: json.pointId,
-  };
+      .then(result => this.makeThrowIf(result === null, validateCodes.DATABASE_NO_RESULT_ERROR))
+      .then(() => database.read(pointsCollection, pointFilter))
 
-  const toUpdate = {
-    pointId: json.point.pointId,
-    pointName: json.point.pointName,
-    pointLongitude: json.point.pointLongitude,
-    pointLatitude: json.point.pointLatitude,
-    pointType: json.point.pointType,
-    pointValue: json.point.pointValue,
-    pointShape: json.point.pointShape,
-    pointIsActive: json.point.pointIsActive,
-  };
-
-  database.update('event_' + json.eventId, filters, { $set: toUpdate })
-    .then(() => {
-      response.send(responseObject);
-    })
-    .catch(error => {
-      utils.responseDatabaseError(response, responseObject, error);
-    });
-}
-
-function PutRequest (request, response) {
-
-  const json = request.body;
-  const error = validator.validate(
-    validator.methods.validatePointPutRequest, json);
-
-  const responseObject = {
-    eventId: json.eventId ? json.eventId : null,
-    error: error,
-  };
-
-  if (error) {
-    response.send(responseObject);
-    return;
+      .then(result => this.makeThrowIf(result === null, validateCodes.DATABASE_NO_RESULT_ERROR))
+      .then(() => database.update(pointsCollection, pointFilter, toUpdate))
+      .then(() => this.sendResponse());
   }
 
-  PutRequestDatabasePart(request, response, responseObject);
+  endpointService () {
+    const json = this.getRequestJson();
+    this.responseObject.eventId = json.eventId ? json.eventId : null;
+    return this.databasePart();
+  }
+
 }
 
 router.get('/', (request, response) => new GetRequestService(request, response, validator.methods.validatePointGetRequest));
 router.post('/', (request, response) => new PostRequestService(request, response, validator.methods.validatePointPostRequest));
-router.put('/', PutRequest);
+router.put('/', (request, response) => new PutRequestService(request, response, validator.methods.validatePointPutRequest));
 module.exports = router;
