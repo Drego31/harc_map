120c751de13625909e32ff0955991a7eafce75d4
Filip Betka <filbetka@gmail.com>
2020-05-01 21:47:51 +0200

    #144: Created get event controller with Endpoint class.

--- controllers/event.js
+++ controllers/event.js
@@ -1,38 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const validator = require('../lib/validator');
+const validateCodes = require('../lib/validateCodes');
 const database = require('../lib/mongodb');
 const utils = require('../lib/utils');
+const Endpoint = require('../lib/endpoint');
 
-router.get('/', (request, response) => {
-  const json = request.query;
-  const error = validator.validate(
-    validator.methods.validateEventGetRequest, json);
+class GetRequestService extends Endpoint {
 
-  let responseObject = {
-    eventId: json.eventId ? json.eventId : null,
-    error: error,
-  };
+  databasePart () {
+    const json = this.getRequestJson();
+    const eventCollection = 'events';
+    const filters = { eventId: json.eventId };
 
-  if (error) {
-    response.send(responseObject);
-    return;
+    return database.read(eventCollection, filters)
+
+      .then(event => {
+        if (event === null) {
+          this.makeThrow(validateCodes.DATABASE_NO_RESULT_ERROR);
+        }
+
+        this.responseObject = Object.assign(this.responseObject, event);
+        delete this.responseObject._id;
+        this.sendResponse();
+      });
   }
 
-  database.read('events', { eventId: json.eventId })
-    .then(result => {
-      if (result === null) {
-        utils.responseDatabaseNoData(response, responseObject);
-        return;
-      }
-      responseObject = Object.assign(responseObject, result);
-      delete responseObject._id;
-      response.send(responseObject);
-    })
-    .catch(error => {
-      utils.responseDatabaseError(response, responseObject, error);
-    });
-});
+  endpointService () {
+    const json = this.getRequestJson();
+    this.responseObject.eventId = json.eventId ? json.eventId : null;
+    return this.databasePart();
+  }
+
+  getRequestJson () {
+    return this.request.query;
+  }
+
+}
 
 router.post('/', (request, response) => {
   const json = request.body;
@@ -102,4 +106,7 @@ router.put('/', (request, response) => {
     });
 });
 
+router.get('/', (request, response) => new GetRequestService(request, response, validator.methods.validateEventGetRequest));
+// router.post('/', (request, response) => new PostRequestService(request, response, validator.methods.validatePointPostRequest));
+// router.put('/', (request, response) => new PutRequestService(request, response, validator.methods.validatePointPutRequest));
 module.exports = router;
