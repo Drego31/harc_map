d24b888ef15e7505e253bc387a6405436ec3b9c7
Filip Betka <filbetka@gmail.com>
2020-05-01 15:48:40 +0200

    #144: Created post point controller with Endpoint class.

--- controllers/eventPoint.js
+++ controllers/eventPoint.js
@@ -12,7 +12,6 @@ class GetRequestService extends Endpoint {
     const pointsCollection = 'event_' + json.eventId;
     const filters = { pointId: json.pointId };
 
-    // prepare response for client
     return database.read(pointsCollection, filters)
 
       .then(point => {
@@ -30,7 +29,6 @@ class GetRequestService extends Endpoint {
     const json = this.getRequestJson();
     this.responseObject.eventId = json.eventId ? json.eventId : null;
     this.responseObject.point = null;
-
     return this.databasePart();
   }
 
@@ -40,85 +38,45 @@ class GetRequestService extends Endpoint {
 
 }
 
+class PostRequestService extends Endpoint {
 
+  databasePart () {
+    const json = this.getRequestJson();
+    const toSave = {
+      pointId: json.point.pointId,
+      pointType: json.point.pointType,
+      pointName: json.point.pointName,
+      pointExpirationTime: json.point.pointExpirationTime,
+      pointCollectionTime: json.point.pointCollectionTime,
+      pointLongitude: json.point.pointLongitude,
+      pointLatitude: json.point.pointLatitude,
+      pointCategory: json.point.pointCategory,
+    };
 
+    const pointsCollection = 'event_' + json.eventId;
+    const eventFilter = { eventId: json.eventId };
+    const pointFilter = { pointId: json.point.pointId };
 
+    return database.read('events', eventFilter)
 
+      .then(result => this.makeThrowIf(result === null, validateCodes.DATABASE_NO_RESULT_ERROR))
+      .then(() => database.read(pointsCollection, pointFilter))
 
-function Throw (validationCode) {
-  const error = new Error();
-  error.name = 'ValidationError';
-  error.validationCode = validationCode;
-  throw error;
-}
-
-function Catch (response, responseObject, error) {
-  if (error.name === 'MongoError') {
-    utils.responseDatabaseError(response, responseObject, error);
-  }
-  if (error.name === 'ValidationError') {
-    responseObject.error = error.validationCode;
-    utils.status(500).send(response, responseObject);
-  }
-}
-
-function PostRequestDatabasePart (request, response, responseObject) {
-
-  const json = request.body;
-  const toSave = {
-    pointId: json.point.pointId,
-    pointType: json.point.pointType,
-    pointName: json.point.pointName,
-    pointExpirationTime: json.pointExpirationTime,
-    pointCollectionTime: json.pointCollectionTime,
-    pointLongitude: json.pointLongitude,
-    pointLatitude: json.pointLatitude,
-    pointCategory: json.pointCategory,
-  };
-
-  const pointsCollection = 'event_' + json.eventId;
-  const eventFilter = { eventId: json.eventId };
-  const pointFilter = { pointId: json.pointId };
-
-  database.read('events', eventFilter)
-
-    .then(result => (result === null) ? Throw(validateCodes.DATABASE_NO_RESULT_ERROR) : undefined)
-    .then(() => database.read(pointsCollection, pointFilter))
-
-    .then(result => (result !== null) ? Throw(validateCodes.DATABASE_DATA_CONFLICT_ERROR) : undefined)
-    .then(() => database.create(pointsCollection, [toSave]))
-    .then(() => response.send(responseObject))
-
-    // error service
-    .catch(error => {
-      if (error.name === 'MongoError') {
-        utils.responseDatabaseError(response, responseObject, error);
-      }
-      if (error.name === 'ValidationError') {
-        responseObject.error = error.validationCode;
-        utils.responseDatabaseNoData(response, responseObject);
-      }
-    });
-}
-
-function PostRequest (request, response) {
-
-  const json = request.body;
-  const error = validator.validate(
-    validator.methods.validatePointPostRequest, json);
-
-  const responseObject = {
-    eventId: json.eventId ? json.eventId : null,
-    error: error,
-  };
-
-  if (error) {
-    response.send(responseObject);
-    return;
+      .then(result => this.makeThrowIf(result !== null, validateCodes.DATABASE_DATA_CONFLICT_ERROR))
+      .then(() => database.create(pointsCollection, [toSave]))
+      .then(() => this.sendResponse());
   }
 
-  PostRequestDatabasePart(request, response, responseObject);
+  endpointService () {
+    const json = this.getRequestJson();
+    this.responseObject.eventId = json.eventId ? json.eventId : null;
+    return this.databasePart();
+  }
+
 }
+
+
+
 
 function PutRequestDatabasePart (request, response, responseObject) {
 
@@ -167,6 +125,6 @@ function PutRequest (request, response) {
 }
 
 router.get('/', (request, response) => new GetRequestService(request, response, validator.methods.validatePointGetRequest));
-router.post('/', PostRequest);
+router.post('/', (request, response) => new PostRequestService(request, response, validator.methods.validatePointPostRequest));
 router.put('/', PutRequest);
 module.exports = router;
