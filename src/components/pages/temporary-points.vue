c22634739faa153c60d3f2379a37b56cbfe9ad47
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-05-28 20:24:33 +0200

    #126 Add temporary points mock for testing

--- src/components/pages/temporary-points.vue
+++ src/components/pages/temporary-points.vue
@@ -1,16 +1,63 @@
 <template>
   <t-page title="Wydarzenia czasowe">
-    1. Wydarzenie czasowe 15:00 - 15:30
+    <o-temporary-points/>
   </t-page>
 </template>
 
 <script>
 import TPage from 'templates/page';
+import OTemporaryPoints from 'organisms/temporary-points';
+
+export const pointDurationTimeMock = 1; // hours
+export const temporaryPointsMock = [
+  {
+    pointId: 'xdXD',
+    pointType: 'timeout',
+    pointName: 'Konstruowanie bomby',
+    pointExpirationTime: new Date(2020, 4, 27, 19, 0, 0), // TODO: Date and time, when point expiration
+    pointCollectionTime: undefined,
+    pointLongitude: -0.12755,
+    pointLatitude: 51.507222,
+    pointCategory: 1,
+  },
+  {
+    pointId: 'xdXM',
+    pointType: 'timeout',
+    pointName: 'Jedzenie nietoperzy',
+    pointExpirationTime: new Date(2020, 4, 27, 20, 0, 0), // TODO: Date and time, when point expiration
+    pointCollectionTime: undefined,
+    pointLongitude: 18.538836,
+    pointLatitude: 54.519502,
+    pointCategory: 1,
+  },
+  {
+    pointId: 'xdUM',
+    pointType: 'timeout',
+    pointName: 'Tych malutkich',
+    pointExpirationTime: new Date(2020, 4, 27, 19, 30, 0), // TODO: Date and time, when point expiration
+    pointCollectionTime: undefined,
+    pointLongitude: 18.538836,
+    pointLatitude: 54.519502,
+    pointCategory: 1,
+  },
+  {
+    pointId: 'xDXM',
+    pointType: 'timeout',
+    pointName: 'Tych dużych też',
+    pointExpirationTime: new Date(2020, 4, 27, 19, 45, 0), // TODO: Date and time, when point expiration
+    pointCollectionTime: undefined,
+    pointLongitude: 18.538836,
+    pointLatitude: 54.519502,
+    pointCategory: 1,
+  },
+];
+// TODO 3 colors of clock related to expiration time
 
 export default {
   name: 'p-collected-points',
   components: {
     TPage,
+    OTemporaryPoints,
   },
 };
 </script>
