1252ae2993908abd649dabf381e40a7a9592718c
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-06-07 19:04:25 +0200

    #126 Add function to modify hours in utils date

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,10 @@
+export const modifyDateHours = (date, modifyValue) => {
+  const modifiedDate = new Date(date);
+  const modifiedDateHour = date.getHours() + modifyValue;
+  return new Date(modifiedDate.setHours(modifiedDateHour));
+
+};
+
 export const getZeroPad = (n) => (parseInt(n, 10) >= 10 ? '' : '0') + n;
 
 export const getMinutesAsString = (date) => `${getZeroPad(date.getMinutes())}`;
@@ -6,4 +13,6 @@ export const getSecondsAsString = (date) => `${getZeroPad(date.getSeconds())}`;
 
 export const getFullDateAsString = (date) => `${date.getHours()}` + ':' + getMinutesAsString(date) + ':' + getSecondsAsString(date);
 
-export const getHoursAndMinutesAsString = (date) => `${date.getHours()}` + ':' + getMinutesAsString(date);
+export const getHoursAndMinutesAsString = (date) => {
+  return `${date.getHours()}` + ':' + getMinutesAsString(date);
+};
