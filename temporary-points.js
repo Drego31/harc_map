12c2a7988e6933e4f7b0ed2379e8d139092ea238
dbetka <dominik.betka@gmail.com>
2020-06-08 15:41:15 +0200

    #127 Add method to generate temporary points for database

---
+++ temporary-points.js
@@ -0,0 +1,37 @@
+/**
+ * 0: category for temporary points
+ *
+ * @returns {({pointLatitude: number, pointCategory: number, pointLongitude: number})[]}
+ */
+
+module.exports =
+  {
+    temporaryPointsForDatabase: function () {
+      return [
+        {
+          pointLatitude: 54.50606,
+          pointLongitude: 18.55436,
+          pointType: 'temporary',
+          pointCategory: 0,
+          pointName: 'Tajemnice jeńca',
+          pointExpirationTime: new Date(),
+        },
+        {
+          pointLatitude: 54.5161,
+          pointLongitude: 18.54206,
+          pointType: 'temporary',
+          pointCategory: 0,
+          pointName: 'Opowieść o holokauście',
+          pointExpirationTime: new Date() + 1000 * 60 * 60, // Date + 1h
+        },
+        {
+          pointLatitude: 54.5228,
+          pointLongitude: 18.5038,
+          pointType: 'temporary',
+          pointCategory: 0,
+          pointName: 'Rota Przyrzeczenia THH',
+          pointExpirationTime: new Date() + 1000 * 60 * 30, // Date + 0.5h
+        },
+      ];
+    },
+  };
