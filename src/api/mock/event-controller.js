c6b9612ac7d1e6d4f0042e720f0311893744725e
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-08-05 20:13:51 +0200

    #189 Create mock for event-controller

--- src/api/mock/event-controller.js
+++ src/api/mock/event-controller.js
@@ -2,23 +2,31 @@ import { getEventByIdMock } from 'api/mock/mockMethods/get-event-by-id';
 import { makeDelayFakeAnswer } from 'api/mock/mock';
 import { ErrorMessage } from 'utils/error-message';
 import { ERRORS } from 'utils/macros/errors';
+import { getPointsByEventIdMock } from 'api/mock/mockMethods/get-points';
 
 export const eventController = {
   getEventById (eventId) {
     if (eventId) {
-      return makeDelayFakeAnswer(getEventByIdMock);
+      return makeDelayFakeAnswer(getEventByIdMock(eventId));
     } else {
       return Promise.reject(new ErrorMessage(ERRORS.eventIdIsRequired));
     }
   },
   getPointsByEventId ({ eventId }) {
-    // 10 points
-    return makeDelayFakeAnswer();
+    if (eventId) {
+      return makeDelayFakeAnswer(getPointsByEventIdMock(eventId));
+    } else {
+      return Promise.reject(new ErrorMessage(ERRORS.eventIdIsRequired));
+    }
   },
   getCategoriesByEventId ({ eventId }) {
     return makeDelayFakeAnswer();
   },
   collectPoint ({ user, eventId, pointId }) {
-    return makeDelayFakeAnswer();
+    return makeDelayFakeAnswer(() => ({
+      error: null,
+      user: user,
+      pointCollectionTime: 1596649311754,
+    }));
   },
 };
