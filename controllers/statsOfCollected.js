42013c4784eebf5b5b465352e99f84cf7e76ebbf
Filip Betka <filbetka@gmail.com>
2020-08-24 00:34:20 +0200

    #191: Added category data for point.

--- controllers/statsOfCollected.js
+++ controllers/statsOfCollected.js
@@ -7,7 +7,13 @@ const Endpoint = require('../lib/endpoint');
 class GetRequestService extends Endpoint {
 
   readCategories (user, point) {
+    const collection = 'point_categories_' + user.userEvents[0];
+    const filters = { categoryId: point.pointCategory };
 
+    return database.read(collection, filters)
+      .then(category => {
+        point.category = category;
+      });
   }
 
   readPoints (user) {
@@ -16,7 +22,16 @@ class GetRequestService extends Endpoint {
 
     return database.readMany(collection, filters)
       .then(points => {
-        return { user, points };
+        const promises = [];
+
+        for (const point of points) {
+          promises.push(this.readCategories(user, point));
+        }
+
+        return Promise.all(promises)
+          .then(() => {
+            return { user, points };
+          });
       });
   }
 
