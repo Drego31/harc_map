adaa2ed40b6f0326f984f3d9aadba604d531fdf7
dbetka <dominik.betka@gmail.com>
2020-08-24 16:13:16 +0200

    #192 Add material-design-icons to project

---
+++ src/components/atoms/icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <i class="a-icon f-material">
+    {{ name }}
+    <slot/>
+  </i>
+</template>
+
+<script>
+export default {
+  name: 'a-icon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
