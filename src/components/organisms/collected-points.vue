de6723d7f425eced83e36de6b1b0588e2b64e3eb
dbetka <dominik.betka@gmail.com>
2020-09-30 12:08:38 +0200

    #207 Prepare new appearance proposition for collected points

--- src/components/organisms/collected-points.vue
+++ src/components/organisms/collected-points.vue
@@ -1,41 +1,78 @@
 <template>
   <div>
-    <m-table-row add-class="f-py-1">
-      <template v-slot:cols>
-        <div/>
-
-        <div class="f-text-bold">
-          Wartość
-        </div>
-
-        <div class="f-text-bold">
-          Ilość
-        </div>
-      </template>
-    </m-table-row>
-
-    <m-table-row-collected-points
-      v-for="category in pointCategories"
-      :key="category.categoryId"
-      :category="category"
-      :quantity="getCollectedPointsLengthById(category.categoryId)"
-      :value="getCollectedPointsValueById(category.categoryId)"
-    />
+    <div class="f-line-24">
+      <div class="f-text-18 f-text-bold f-mb-1">Suma punktów wg kategorii</div>
+      <div class="m-grid f-category-sum f-mb-1" style="border-bottom: 1px solid gray">
+        <div>Kategoria</div>
+        <div>Ilość zebranych</div>
+        <div>Suma wartości</div>
+      </div>
+      <div class="m-grid f-category-sum">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-danger" /></div>
+        <div>40</div>
+        <div>320 pkt</div>
+      </div>
+      <div class="m-grid f-category-sum">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-warning" /></div>
+        <div>40</div>
+        <div>320 pkt</div>
+      </div>
+      <div class="m-grid f-category-sum">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-info" /></div>
+        <div>40</div>
+        <div>320 pkt</div>
+      </div>
+    </div>
+    <div class="f-pt-3 f-line-24">
+      <div class="f-text-18 f-text-bold f-mb-1">Lista zebranych punktów</div>
+      <div class="m-grid f-point f-mb-1" style="border-bottom: 1px solid gray">
+        <div>Kat.</div>
+        <div>Kod</div>
+        <div>Wartość</div>
+        <div>Miejsce</div>
+        <div>Rozwiń</div>
+      </div>
+      <div class="m-grid f-point">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-info" /></div>
+        <div>ksf0</div>
+        <div>1 pkt</div>
+        <div><a-icon :name="ICONS.map" /></div>
+        <div><a-icon :name="ICONS.arrow_drop_down"/></div>
+      </div>
+      <div class="m-grid f-point">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-info" /></div>
+        <div>ksf0</div>
+        <div>1 pkt</div>
+        <div><a-icon :name="ICONS.map" /></div>
+        <div><a-icon :name="ICONS.arrow_drop_down"/></div>
+      </div>
+      <div class="m-grid f-point">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-warning" /></div>
+        <div>ksf0</div>
+        <div>3 pkt</div>
+        <div><a-icon :name="ICONS.map" /></div>
+        <div><a-icon :name="ICONS.arrow_drop_down"/></div>
+      </div>
+      <div class="m-grid f-point">
+        <div><a-icon :name="ICONS.stop_circle" class="f-text-danger" /></div>
+        <div>ksf0</div>
+        <div>7 pkt</div>
+        <div><a-icon :name="ICONS.map" /></div>
+        <div><a-icon :name="ICONS.arrow_drop_down"/></div>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import MTableRow from 'molecules/table-row';
-import MTableRowCollectedPoints from 'molecules/table-row/collected-points';
+import AIcon from 'atoms/icon';
 
 export default {
   name: 'o-collected-points',
   components: {
-    MTableRowCollectedPoints,
-    MTableRow,
-
+    AIcon,
   },
   data: () => ({
     pointCategories: [
