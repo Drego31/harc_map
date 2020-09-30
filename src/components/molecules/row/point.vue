5e408c704a5538d8ef31d00d34055970b8168368
dbetka <dominik.betka@gmail.com>
2020-09-30 14:54:25 +0200

    #207 Refactor collected points view

---
+++ src/components/molecules/row/point.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="m-grid f-point">
+    <div>
+      <a-icon-category :category-id="point.pointCategory"/>
+    </div>
+    <div>{{ point.pointId }}</div>
+    <div>{{ getCategoryById(point.pointCategory).pointValue }} pkt</div>
+    <div>
+      <a-icon :name="ICONS.map"/>
+    </div>
+    <div>
+      <a-icon :name="ICONS.arrow_drop_down"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AIcon from 'atoms/icon';
+import AIconCategory from 'atoms/icon/category';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'm-row-point',
+  components: {
+    AIconCategory,
+    AIcon,
+  },
+  props: {
+    point: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters('event', [
+      'getCategoryById',
+    ]),
+  },
+};
+</script>
--- src/components/organisms/collected-points.vue
+++ src/components/organisms/collected-points.vue
@@ -1,50 +1,32 @@
 <template>
   <div>
     <div class="f-line-24">
-      <div class="f-text-18 f-text-bold f-mb-1">Suma punktów wg kategorii</div>
-      <div class="m-grid f-category-sum f-mb-1" style="border-bottom: 1px solid gray">
+      <div class="a-text f-title f-table">Suma punktów wg kategorii</div>
+      <div class="m-row f-header f-category-sum">
         <div>Kategoria</div>
         <div>Ilość zebranych</div>
         <div>Suma wartości</div>
       </div>
-      <div
+      <m-row-category-sum
         v-for="category in categories"
         :key="category.categoryId"
-        class="m-grid f-category-sum"
-      >
-        <div>
-          <a-icon :name="ICONS.stop_circle" :class="getCategoryClassById(category.categoryId)"/>
-        </div>
-        <div>{{ getCollectedPointsLengthById(category.categoryId) }}</div>
-        <div>{{ getCollectedPointsValueById(category.categoryId) }} pkt</div>
-      </div>
+        :category="category"
+      />
     </div>
     <div class="f-pt-3 f-line-24">
-      <div class="f-text-18 f-text-bold f-mb-1">Lista zebranych punktów</div>
-      <div class="m-grid f-point f-mb-1" style="border-bottom: 1px solid gray">
+      <div class="a-text f-title f-table">Lista zebranych punktów</div>
+      <div class="m-row f-header f-point">
         <div>Kat.</div>
         <div>Kod</div>
         <div>Wartość</div>
         <div>Miejsce</div>
         <div>Rozwiń</div>
       </div>
-      <div
+      <m-row-point
         v-for="point of collectedPoints"
         :key="point.pointId"
-        class="m-grid f-point"
-      >
-        <div>
-          <a-icon :name="ICONS.stop_circle" :class="getCategoryClassById(point.pointCategory)"/>
-        </div>
-        <div>{{ point.pointId }}</div>
-        <div>{{ getCategoryById(point.pointCategory).pointValue }} pkt</div>
-        <div>
-          <a-icon :name="ICONS.map"/>
-        </div>
-        <div>
-          <a-icon :name="ICONS.arrow_drop_down"/>
-        </div>
-      </div>
+        :point="point"
+      />
     </div>
 
   </div>
@@ -52,12 +34,14 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import AIcon from 'atoms/icon';
+import MRowCategorySum from 'molecules/row/category-sum';
+import MRowPoint from 'molecules/row/point';
 
 export default {
   name: 'o-collected-points',
   components: {
-    AIcon,
+    MRowPoint,
+    MRowCategorySum,
   },
   computed: {
     ...mapGetters('user', [
@@ -65,28 +49,7 @@ export default {
     ]),
     ...mapGetters('event', [
       'categories',
-      'getCategoryById'
     ]),
-  },
-  methods: { // :TODO: Change pointShape to categoryId after refactor points in API
-    getCategoryClassById (categoryId) {
-      const pointShape = this.getCategoryById(categoryId).pointShape;
-      switch (pointShape) {
-        case 1:
-          return 'f-text-info';
-        case 2:
-          return 'f-text-warning';
-        case 3:
-          return 'f-text-danger';
-      }
-    },
-    getCollectedPointsLengthById (categoryId) {
-      return this.collectedPoints.filter(point => point.pointCategory === categoryId).length;
-    },
-    getCollectedPointsValueById (categoryId) {
-      const length = this.collectedPoints.filter(point => point.pointCategory === categoryId).length;
-      return length * this.getCategoryById(categoryId).pointValue;
-    },
   },
 };
 </script>
