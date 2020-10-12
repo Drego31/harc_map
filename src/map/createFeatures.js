9193022f171ac88edbcd4c377ecdfb584b542bb2
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-10-12 13:45:59 +0200

    #68 Add first working version for drawing lines at map

--- src/map/createFeatures.js
+++ src/map/createFeatures.js
@@ -8,6 +8,8 @@ import { Fill, RegularShape, Stroke, Style } from 'ol/style';
 import { MAP_POINTS } from 'utils/macros/map-point-types';
 import { store } from 'store';
 import { uCheck } from '@dbetka/utils';
+import { getCollectedPointsByMock } from 'api/mock/mockMethods/get-collected-points';
+import LineString from 'ol/geom/LineString';
 
 const getStroke = (shape, width = 2) => {
   const appearance = MAP_POINTS[shape] || {};
@@ -74,11 +76,42 @@ export function createFeatures ({ list = [] }) {
   }
   store.commit('event/updateListOfPoints', list);
 
+  /* TESTING */
+  const pointsMock = getCollectedPointsByMock();
+
+  const linePath = [
+    [pointsMock[0].longitude, pointsMock[0].latitude],
+    [pointsMock[1].longitude, pointsMock[1].latitude],
+    [pointsMock[2].longitude, pointsMock[2].latitude],
+    [pointsMock[3].longitude, pointsMock[3].latitude],
+    [pointsMock[4].longitude, pointsMock[4].latitude],
+    [pointsMock[5].longitude, pointsMock[5].latitude],
+  ];
+  const polyline = new LineString(linePath);
+  polyline.transform('EPSG:4326', 'EPSG:3857');
+  const lineFeature = new Feature(polyline);
+  console.log(lineFeature);
+  /* TESTING */
+
   const layer = new VectorLayer({
     source: new VectorSource({
       features: listOfFeatures,
     }),
   });
+  const lineLayer = new VectorLayer({
+    source: new VectorSource({
+      features: [lineFeature],
+    }),
+    style: new Style({
+      stroke: new Stroke(
+        {
+          color: '#32f306',
+          width: 5,
+        },
+      ),
+    }),
+  });
+  map.realMap.addLayer(lineLayer);
   map.realMap.addLayer(layer);
   map.points.layer = layer;
 }
