cc8dc9ee7d3be52f39b2d13ad882077939ad6184
dbetka <dominik.betka@gmail.com>
2020-11-26 21:35:32 +0100

    #218 Set page title based on currentRoute - not props

--- src/components/templates/page.vue
+++ src/components/templates/page.vue
@@ -5,18 +5,15 @@
 </template>
 
 <script>
+import { ROUTES } from 'utils/macros/routes';
+
 export default {
   name: 't-page',
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-  },
   mounted () {
-    this.$store.commit('header/setPageTitle', this.title);
+    const title = ROUTES[this.$router.currentRoute.name].label;
+    this.$store.commit('header/setPageTitle', title);
     if (this.title) {
-      document.title = `${this.title} - ${APP_NAME}`;
+      document.title = `${title} - ${APP_NAME}`;
     } else {
       document.title = APP_NAME;
     }
