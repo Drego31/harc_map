b9f48237e4a7a2a90e5d0f53d05eead8327705a1
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-11-29 18:18:16 +0100

    Move o-sign-up to p-sign-up and remove organism

--- src/components/pages/sign-up.vue
+++ src/components/pages/sign-up.vue
@@ -1,18 +1,99 @@
 <template>
   <t-page>
-    <o-form-sign-up/>
+    <o-form
+      :is-send="formSend"
+      :on-submit="signUp"
+    >
+      <template slot="form">
+        <m-field-email
+          v-model="values.user"
+          :disabled="blockForm"
+        />
+        <m-field-set-password
+          v-model="values.password"
+          :disabled="blockForm"
+        />
+        <m-field-text
+          :label="$t('form.field.userTeam')"
+          :rules="rules.userTeam"
+          v-model="values.userTeam"
+          :disabled="blockForm"
+        />
+        <m-field-text
+          :label="$t('form.field.eventId')"
+          :rules="rules.eventId"
+          v-model="values.eventId"
+          :disabled="blockForm"
+        />
+        <a-button-submit
+          :disabled="blockForm"
+          :is-sending="isSending"
+          :message="message"
+        />
+      </template>
+
+      <template slot="response">
+        <div class="f-py-2">
+          <div class="f-pb-2 f-bold">Rejestracja przebiegła pomyślnie!</div>
+          Link aktywacyjny został wysłany na wskazany adres e-mail:
+          <span class="f-bold">{{ values.user }}</span>
+        </div>
+        <a-button-primary @click="$router.push(ROUTES.signIn.path)">
+          Przejdź do logowania
+        </a-button-primary>
+      </template>
+    </o-form>
   </t-page>
 </template>
 
 <script>
 import TPage from 'templates/page';
-import OFormSignUp from 'organisms/form/sign-up';
+import { api } from 'api/index';
+import { mixins } from 'mixins/base';
+import AButtonSubmit from 'atoms/button/submit';
+import MFieldEmail from 'molecules/field/email';
+import MFieldSetPassword from 'molecules/field/set-password';
+import MFieldText from 'molecules/field/text';
+import OForm from 'organisms/form';
+import AButtonPrimary from 'atoms/button/primary';
 
 export default {
   name: 'p-sign-up',
+  mixins: [mixins.form, mixins.validation],
   components: {
-    OFormSignUp,
     TPage,
+    AButtonPrimary,
+    OForm,
+    MFieldText,
+    MFieldSetPassword,
+    MFieldEmail,
+    AButtonSubmit,
+  },
+  data: () => ({
+    values: {
+      user: '',
+      password: '',
+      userTeam: '',
+      eventId: '',
+    },
+    blockForm: false,
+    isSending: false,
+    message: '',
+    formSend: false,
+  }),
+  methods: {
+    onSignUp () {
+      this.formSend = true;
+      this.isSending = false;
+      this.blockForm = false;
+    },
+    signUp () {
+      this.isSending = true;
+      this.blockForm = true;
+      api.signUp(this.values)
+        .then(this.onSignUp)
+        .catch(this.onError);
+    },
   },
 };
 </script>
