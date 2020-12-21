eba0efcd750ff061b9bee1482e2aa55003836cd1
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-12-21 20:46:36 +0100

    #196 Working version of timer

---
+++ src/components/molecules/countdown-timer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :style= "[minutes === 0 ? {color: 'red'} : {color: 'black'}]">
+    {{getMinutes}}:{{getSeconds}}
+  </div>
+</template>
+
+<script>
+import { getZeroPad } from 'utils/date';
+
+export default {
+  name: 'm-countdown-timer',
+  data: () => ({
+    minutes: 0,
+    seconds: 5,
+  }),
+  mounted () {
+    this.setStartTime();
+    this.updateDateTime();
+    this.$options.interval = setInterval(this.updateDateTime, 1000);
+  },
+  beforeDestroy () {
+    clearInterval(this.$options.interval);
+  },
+  computed: {
+    getSeconds () {
+      if (this.seconds < 0) return '00';
+      return getZeroPad(this.seconds);
+    },
+    getMinutes () {
+      return getZeroPad(this.minutes);
+    },
+  },
+  methods: {
+    updateDateTime () {
+      if (this.seconds === 0) {
+        if (this.minutes === 0) {
+          this.minutes = 14;
+          this.seconds = 59;
+        } else {
+          this.minutes -= 1;
+          this.seconds = 59;
+        }
+      } else {
+        this.seconds -= 1;
+      }
+    },
+    setStartTime () {
+      const now = new Date();
+      const minutesAfterGap = now.getMinutes() % 15;
+      const secondsAfterGap = now.getSeconds();
+      this.minutes = 15 - minutesAfterGap;
+      this.seconds = 59 - secondsAfterGap;
+    },
+  },
+};
+</script>
--- src/components/pages/map.vue
+++ src/components/pages/map.vue
@@ -1,16 +1,20 @@
 <template>
   <t-page title="Mapa" class="f-map">
-    <o-map/>
+    <o-map>
+    </o-map>
+    <m-banner-timer/>
   </t-page>
 </template>
 
 <script>
 import TPage from 'templates/page';
 import OMap from 'organisms/map';
+import MBannerTimer from 'molecules/banner-timer';
 
 export default {
   name: 'p-map',
   components: {
+    MBannerTimer,
     OMap,
     TPage,
   },
