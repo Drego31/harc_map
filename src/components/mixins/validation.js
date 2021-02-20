e277d16389e513554dc19ffa75bb07a02ee605f2
dbetka <dominik.betka@gmail.com>
2021-02-20 10:36:02 +0100

    #283 Create date input

--- src/components/mixins/validation.js
+++ src/components/mixins/validation.js
@@ -1,12 +1,13 @@
 export const validationMixin = {
   data: () => ({
-    rules: {
+    rules: { // based on vee-validate rules
       email: 'required|email',
       password: 'required|min:8|max:64|hasNumber|hasCapitalize',
       passwordConfirmation: 'required|confirmed:password',
       userTeam: 'required|min:4',
       eventId: 'required|length:4',
       pointId: 'required|length:4',
+      date: 'required',
     },
   }),
 };
