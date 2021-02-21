739aec1f5598ab59f27d78356d86adf70e03b2ff
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-02-21 22:46:28 +0100

    #269 Add success message to banner-map component

--- src/components/molecules/banner-map.vue
+++ src/components/molecules/banner-map.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="m-banner-map">
-    <div class="f-flex-1">
-      <slot name="text"></slot>
-    </div>
-    <a-button-primary
-      class="f-flex-0 f-m-0"
-      :add-class="'min'"
-      @click="$emit('actionTriggered')"
-    >
-      <slot name="button-name"></slot>
-    </a-button-primary>
+  <div>
+    <transition name="fade">
+      <div v-if="showSuccessMessage === false" class="m-banner-map">
+        <div class="f-flex-1">
+          <slot name="text"></slot>
+        </div>
+        <a-button-primary
+          class="f-flex-0 f-m-0"
+          :add-class="'min'"
+          @click="$emit('actionTriggered')"
+        >
+          <slot name="button-name"></slot>
+        </a-button-primary>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div v-if="showSuccessMessage"
+           class="m-banner-map f-message-success">
+        <span class="f-text-center">
+          <slot name="success-message">
+            Nowa pozycja została zapisana.
+          </slot>
+        </span>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -19,6 +33,23 @@ import AButtonPrimary from 'atoms/button/primary';
 export default {
   name: 'm-banner-map',
   components: { AButtonPrimary },
+  data: () => ({
+    showSuccessMessage: false,
+  }),
+  props: {
+    successMessageDuration: {
+      type: Number,
+      default: 1500,
+    },
+  },
+  methods: {
+    emitSuccessMessage () {
+      this.showSuccessMessage = true;
+      window.setTimeout(() => {
+        this.showSuccessMessage = false;
+      }, this.successMessageDuration);
+    },
+  },
 };
 </script>
 
