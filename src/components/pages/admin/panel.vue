148b9810fdf5fbcd2d1c8756159ef12dfe8fff59
dbetka <dominik.betka@gmail.com>
2021-02-24 22:46:02 +0100

    #296 Create admin panel page and link button for it

---
+++ src/components/pages/admin/panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <t-page>
+    Admin Panel
+  </t-page>
+</template>
+
+<script>
+import TPage from 'templates/page';
+
+export default {
+  name: 'p-admin-panel',
+  components: {
+    TPage,
+  },
+  data: () => ({}),
+};
+</script>
