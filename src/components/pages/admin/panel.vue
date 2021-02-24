556b5dd20e4c184324acff2cc376524af2d00d44
dbetka <dominik.betka@gmail.com>
2021-02-24 23:03:19 +0100

    #296 Add list of links to admin panel

--- src/components/pages/admin/panel.vue
+++ src/components/pages/admin/panel.vue
@@ -1,17 +1,35 @@
 <template>
-  <t-page>
-    Admin Panel
+  <t-page class="f-text-center">
+
+    <a-button-primary
+      v-for="route of links"
+      :key="route.name"
+      @click="$router.push(route)"
+    >
+      {{ route.label }}
+    </a-button-primary>
   </t-page>
 </template>
 
 <script>
 import TPage from 'templates/page';
+import AButtonPrimary from 'atoms/button/primary';
+import { ROUTES } from 'utils/macros/routes';
 
 export default {
   name: 'p-admin-panel',
   components: {
+    AButtonPrimary,
     TPage,
   },
-  data: () => ({}),
+  computed: {
+    links () {
+      return [
+        ROUTES.collectedPoints,
+        ROUTES.editEvent,
+        ROUTES.setMapPosition,
+      ];
+    },
+  },
 };
 </script>
