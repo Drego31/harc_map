bebaf463a903d1547b7f8fa024f5f384ae824d44
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-02-25 23:01:03 +0100

    #275 temporary work around for timeout point macros and macros for point categories

--- src/utils/macros.js
+++ src/utils/macros.js
@@ -2,6 +2,29 @@ export const MACROS = {
   pointType: {
     temporary: 'temporary',
     permanent: 'permanent',
+    timeout: 'timeout',
   },
   pointDurationTime: 1, // 1000 * 60 * 60 temporary in hours
+  pointCategory: [
+    /*    {
+      categoryId: 0,
+      pointValue: 0,
+      pointShape: 0,
+    }, */
+    {
+      categoryId: 1,
+      pointValue: 1,
+      pointShape: 1,
+    },
+    {
+      categoryId: 2,
+      pointValue: 2,
+      pointShape: 2,
+    },
+    {
+      categoryId: 3,
+      pointValue: 3,
+      pointShape: 3,
+    },
+  ],
 };
