4bd3313389fd02f80ec65890b88f4944a2ee0540
dbetka <dominik.betka@gmail.com>
2021-03-01 16:50:38 +0100

    #301 Add content generated of list to o-popup-map

--- src/components/organisms/popup/map.vue
+++ src/components/organisms/popup/map.vue
@@ -1,21 +1,14 @@
 <template>
   <div ref="mapPopup" class="o-popup f-map">
     <div
-      @click="$router.push(ROUTES.adminPanel)"
+      v-for="button of buttons"
+      :key="button.label"
+      @click="button.method()"
       class="f-flex"
     >
-      <a-icon :name="ICONS.edit"/>
+      <a-icon :name="button.icon"/>
       <div class="f-flex-1 f-pl-1">
-        Edycja
-      </div>
-    </div>
-    <div
-      @click="popup.hide()"
-      class="f-flex"
-    >
-      <a-icon :name="ICONS.close"/>
-      <div class="f-flex-1 f-pl-1">
-        Ukryj
+        {{ button.label }}
       </div>
     </div>
   </div>
@@ -31,6 +24,22 @@ export default {
   data: () => ({
     popup: null,
   }),
+  computed: {
+    buttons () {
+      return [
+        {
+          icon: this.ICONS.edit,
+          label: 'Edycja',
+          method: () => this.$router.push(this.ROUTES.adminPanel),
+        },
+        {
+          icon: this.ICONS.close,
+          label: 'Ukryj',
+          method: () => this.popup.hide(),
+        },
+      ];
+    },
+  },
   methods: {
     definePopup () {
       this.popup = new Popup({
