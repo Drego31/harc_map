afa54c829726bcd70b0bd3b11a7ef73443bc5bde
dbetka <dominik.betka@gmail.com>
2021-03-03 15:33:00 +0100

    #241 Create circle progress and add it to scoreboard

--- src/components/pages/admin/scoreboard.vue
+++ src/components/pages/admin/scoreboard.vue
@@ -1,5 +1,27 @@
 <template>
   <t-page class="f-text-center">
+    <div class="a-text f-title f-table">Poziom realizacji gry</div>
+    <div class="f-pt-1">
+      <m-circle-progress
+        :progress="33"
+        :max-range="50"
+        :color="$store.getters['theme/colors'].info"
+      />
+      <m-circle-progress
+        class="f-mx-1"
+        :progress="15"
+        :max-range="40"
+        :color="$store.getters['theme/colors'].warning"
+      />
+      <m-circle-progress
+        :progress="10"
+        :max-range="20"
+        :color="$store.getters['theme/colors'].danger"
+      />
+    </div>
+    <div class="f-pt-1 f-pb-3 f-text-subtext f-text-14">
+      Wykresy przedstawiają ilość zebranych punktów przez uczestników z podziałem na kategorie.
+    </div>
     <div class="f-line-24">
       <div class="a-text f-title f-table">{{ $t('page.collectedPoints.scoreTitle') }}</div>
       <div class="m-row f-header f-score">
@@ -26,10 +48,12 @@
 import TPage from 'templates/page';
 import { mapGetters } from 'vuex';
 import MRowScore from 'molecules/row/score';
+import MCircleProgress from 'molecules/circle-progress';
 
 export default {
   name: 'p-scoreboards',
   components: {
+    MCircleProgress,
     MRowScore,
     TPage,
   },
