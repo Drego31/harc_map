d92bff474177d76803fcc64f21635434b0b52c8c
dbetka <dominik.betka@gmail.com>
2021-03-15 18:33:43 +0100

    #370 Create ground for welcome guide

---
+++ src/components/organisms/guide.vue
@@ -0,0 +1,21 @@
+<template>
+  <o-float-container>
+    <m-slide
+      title="Sprawdź czas rozpoczęcia wydarzenia"
+      description="Wydarzenie, na które się zarejestrowałeś rozpoczyna się konkretnego dnia i konkretnej godzinie.
+      Informację o tym znajdziesz na stronie startowej zaraz pod zdjęciem wydarzenia."
+      :icon="ICONS.timer"
+      slide-position="0"
+      slide-max="4"
+    />
+  </o-float-container>
+</template>
+
+<script>
+import OFloatContainer from 'organisms/float-container';
+import MSlide from 'molecules/slide';
+export default {
+  name: 'o-guide',
+  components: { MSlide, OFloatContainer },
+};
+</script>
