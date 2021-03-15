e7ae4f03f28717117638d93c17cb0cf871d7e6ef
dbetka <dominik.betka@gmail.com>
2021-03-15 18:54:59 +0100

    #370 Add external slider component

--- src/components/molecules/slide.vue
+++ src/components/molecules/slide.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="f-relative f-p-2 f-flex f-flex-col f-height-100">
-    <a-icon-close-popup add-class="f-mt-1 f-mr-1"/>
+  <div class="m-slide">
     <div class="a-title f-size-28 f-mt-3">{{ title }}</div>
     <div>{{ description }}</div>
     <div class="f-flex-1"></div>
-    <div class="f-mt-3 f-mb-6 f-text-primary f-text-center">
+    <div class="f-mt-3 f-mb-6 f-pb-6 f-text-primary f-text-center">
       <a-icon
         :name="icon"
         size="150"
       />
     </div>
-    <div class="f-py-2">slider no {{ slidePosition }}</div>
-    <div class="f-py-2">poprzedni - następny</div>
   </div>
 </template>
 
 <script>
-import AIconClosePopup from 'atoms/icon/close-popup';
 import AIcon from 'atoms/icon';
 export default {
   name: 'm-slide',
-  components: { AIcon, AIconClosePopup },
+  components: { AIcon },
   props: {
     title: { type: String, required: true },
     description: { type: String, required: true },
     icon: { type: String, required: true },
-    slidePosition: { type: [String, Number], required: true },
   },
 };
 </script>
--- src/components/organisms/guide.vue
+++ src/components/organisms/guide.vue
@@ -1,21 +1,62 @@
 <template>
   <o-float-container>
-    <m-slide
-      title="Sprawdź czas rozpoczęcia wydarzenia"
-      description="Wydarzenie, na które się zarejestrowałeś rozpoczyna się konkretnego dnia i konkretnej godzinie.
-      Informację o tym znajdziesz na stronie startowej zaraz pod zdjęciem wydarzenia."
-      :icon="ICONS.timer"
-      slide-position="0"
-      slide-max="4"
-    />
+    <a-icon-close-popup add-class="f-mt-1 f-mr-1"/>
+    <slider ref="slider" :options="options">
+      <!-- slideritem wrapped package with the components you need -->
+      <slider-item v-for="(slide, index) in slides" :key="index">
+        <m-slide
+          :title="slide.title"
+          :description="slide.description"
+          :icon="slide.icon"
+        />
+      </slider-item>
+      <!-- Customizable loading -->
+      <div slot="loading">loading...</div>
+    </slider>
+
   </o-float-container>
 </template>
 
 <script>
+import { slider as Slider, slideritem as SliderItem } from 'vue-concise-slider';
 import OFloatContainer from 'organisms/float-container';
 import MSlide from 'molecules/slide';
+import AIconClosePopup from 'atoms/icon/close-popup';
+
 export default {
   name: 'o-guide',
-  components: { MSlide, OFloatContainer },
+  components: {
+    AIconClosePopup,
+    Slider,
+    SliderItem,
+    MSlide,
+    OFloatContainer,
+  },
+  data: () => ({
+    options: {
+      currentPage: 0,
+    },
+  }),
+  computed: {
+    slides () {
+      return [
+        {
+          title: 'Sprawdź czas rozpoczęcia wydarzenia',
+          description: 'Wydarzenie, na które się zarejestrowałeś rozpoczyna się konkretnego dnia i konkretnej godzinie. Informację o tym znajdziesz na stronie startowej zaraz pod zdjęciem wydarzenia.',
+          icon: this.ICONS.timer,
+        },
+        {
+          title: 'Rozpocznij poszukiwania',
+          description: 'Wydarzenie, na które się zarejestrowałeś rozpoczyna się konkretnego dnia i konkretnej godzinie. Informację o tym znajdziesz na stronie startowej zaraz pod zdjęciem wydarzenia.',
+          icon: this.ICONS.score,
+        },
+        {
+          title: 'Wypatruj wydarzeń czasowych',
+          description: 'Wydarzenie, na które się zarejestrowałeś rozpoczyna się konkretnego dnia i konkretnej godzinie. Informację o tym znajdziesz na stronie startowej zaraz pod zdjęciem wydarzenia.',
+          icon: this.ICONS.watch_later,
+        },
+      ];
+    },
+  },
 };
 </script>
