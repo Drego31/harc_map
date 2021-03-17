448cf51fb6c66b6ced2cf961604f93dd4398a5b1
dbetka <dominik.betka@gmail.com>
2021-03-17 15:23:11 +0100

    #221 Use font files from google server and clean up code

--- bin/create-list-of-icons.js
+++ bin/create-list-of-icons.js
@@ -1,16 +1,17 @@
 const { generateListOfIcons, writeListOfIconsIntoFile } = require('../lib/icon-list-generator');
-
+const { FgGreen, FgCyan } = require('../vendors/console-colors');
 const iconListSource = 'https://fonts.google.com/metadata/icons';
 const iconListDestination = './src/__jscash__/icons-names-list.js';
 
 function createListOfIconsFile () {
-  console.log('\nDownloading list of icons...');
+  console.log(FgCyan + '\nDownloading list of icons...');
   return generateListOfIcons(iconListSource)
     .then(list => writeListOfIconsIntoFile(iconListDestination, list))
     .then(() => {
       const splitSourcePath = iconListSource.split('/');
       const fileName = splitSourcePath[splitSourcePath.length - 1];
-      console.log(fileName + ' ~> ' + iconListDestination);
+      console.log(FgCyan + fileName + ' ~> ' + iconListDestination);
+      console.log(FgGreen + 'Done!');
     });
 }
 
