05353fdc312a7d5336c64f8bb51a63acbb0b026d
dbetka <dominik.betka@gmail.com>
2021-03-19 23:20:05 +0100

    #398 Add search-point page appearance

--- src/components/pages/admin/search-point.vue
+++ src/components/pages/admin/search-point.vue
@@ -2,13 +2,24 @@
   <t-page>
     <div>
       <m-field-text
-        label="Szukaj po kodzie..."
+        label="Szukaj..."
+        assist="Szukaj po kodzie lub nazwie punktu."
         v-model="phrase"
       />
     </div>
     <div>
-      <div v-for="[key, point] of filteredPoints.entries()" :key="key">
-        {{ point.pointId }}
+      <div
+        class="m-grid f-search-point"
+        v-for="[key, point] of filteredPoints.entries()"
+        :key="key"
+      >
+        <div class="f-py-2">{{ point.pointId }}</div>
+        <div class="f-pl-1 f-py-2 f-text-subtext f-text-14">
+          {{ point.pointName }}
+        </div>
+        <a-button-icon @click="panToMap(point)">
+          <a-icon :name="ICONS.map"/>
+        </a-button-icon>
       </div>
     </div>
   </t-page>
@@ -19,10 +30,15 @@ import * as JsSearch from 'js-search';
 import { mapGetters } from 'vuex';
 import TPage from 'templates/page';
 import MFieldText from 'molecules/field/text';
+import AIcon from 'atoms/icon';
+import { map } from 'map';
+import AButtonIcon from 'atoms/button/icon';
 
 export default {
   name: 'p-search-point',
   components: {
+    AButtonIcon,
+    AIcon,
     MFieldText,
     TPage,
   },
@@ -42,6 +58,11 @@ export default {
     filteredPoints () {
       const searched = this.searcher.search(this.phrase);
       return this.phrase === '' ? this.points : searched;
+    },
+  },
+  methods: {
+    panToMap (point) {
+      map.panToPointLocationOnMap(point);
     },
   },
   watch: {
