b09a7ad2ec6098f311b1e1d870bfa31f94b5ce96
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-05-09 11:54:53 +0200

    Add event store comment related to #375 issue

--- src/store/event-modules/index.js
+++ src/store/event-modules/index.js
@@ -2,7 +2,12 @@ import pointStatistic from 'store/event-modules/points-statistics';
 import categories from 'store/event-modules/categories';
 import map from 'store/event-modules/map';
 import points from 'store/event-modules/points';
-
+/*
+  We decided to use pure js modules upon vuex modules,
+  because event store is more like one big module than group of independent submodules
+  and we didn't want to using 'rootGetters' etc.
+  We did spread manually for vuex to recognize it as part of event-store.
+*/
 export const eventStoreModules = {
   state: {
     ...pointStatistic.state,
